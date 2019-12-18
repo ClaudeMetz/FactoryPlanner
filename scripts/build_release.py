128e4d688fea1ac8e64ef081dad0a54fd2a93233
Claude Metz <claudearmetz@gmail.com>
2019-12-18 14:13:41 +0100

    Remove windows scripts

--- scripts/build_release.py
+++ scripts/build_release.py
@@ -7,10 +7,8 @@
 import fileinput
 import itertools
 import json
-import platform
 import re
 import shutil
-import subprocess
 import sys
 from datetime import datetime
 from pathlib import Path
@@ -20,7 +18,6 @@ import git  # gitpython module
 # Script config
 MODNAME = sys.argv[1]
 
-os = platform.system()
 cwd = Path.cwd()
 repo = git.Repo(cwd / MODNAME)
 
@@ -40,17 +37,10 @@ def build_release():
     print("- info.json version bumped")
 
     # Update factorio folder mod symlink
-    if os == "Darwin":
-        mods_path = cwd / "userdata" / "mods"
-        old_mod_symlink = list(itertools.islice(mods_path.glob(MODNAME + "_*"), 1))[0]
-        new_mod_symlink = Path(mods_path, MODNAME + "_" + new_mod_version)
-        old_mod_symlink.rename(new_mod_symlink)
-    else:  # os == "Windows"
-        mods_path = list(itertools.islice(cwd.glob("Factorio_*"), 1))[0] / "mods"
-        old_mod_symlink = list(itertools.islice(mods_path.glob(MODNAME + "_*"), 1))[0]
-        old_mod_symlink.rmdir()
-        new_mod_symlink = Path(mods_path, MODNAME + "_" + new_mod_version)
-        subprocess.run(["mklink", "/J", str(new_mod_symlink), str(modfiles_path), ">nul"], shell=True)
+    mods_path = cwd / "userdata" / "mods"
+    old_mod_symlink = list(itertools.islice(mods_path.glob(MODNAME + "_*"), 1))[0]
+    new_mod_symlink = Path(mods_path, MODNAME + "_" + new_mod_version)
+    old_mod_symlink.rename(new_mod_symlink)
     print("- mod folder symlink updated")
 
     # Disable devmode if it is active
--- scripts/macos.sh
+++ scripts/macos.sh
@@ -4,12 +4,9 @@
 # It runs the selected python script in the appropriate directory
 
 origin=$(pwd)
-# Detect the modname using the name of the parent folder
+
 cd ".."
 modname=${PWD##*/}
-
-cd "./scripts/code"
-codedir=$(pwd)
 
 echo "[1] New changelog entry"
 echo "[2] Switch branch"
@@ -17,19 +14,18 @@ echo "[3] Build release"
 echo -e "Select script to run: \c"
 read choice
 
-# This will error if choice is not a number
 if [ $choice -eq 1 ]
 then
-    cd "../../modfiles/"
-    script="${codedir}/new_changelog_entry.py"
+    cd "modfiles/"
+    script="${origin}/new_changelog_entry.py"
 elif [ $choice -eq 2 ]
 then
-    cd "../../../"
-    script="${codedir}/switch_branch.py"
+    cd "../"
+    script="${origin}/switch_branch.py"
 elif [ $choice -eq 3 ]
 then
-    cd "../../../"
-    script="${codedir}/build_release.py"
+    cd "../"
+    script="${origin}/build_release.py"
 else
     exit
 fi
