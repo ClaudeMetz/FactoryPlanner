c7e3443dac5a472affba04e4f598af39400ea388
Claude Metz <claudearmetz@gmail.com>
2020-06-04 14:00:01 +0200

    Add migration so fuel choices don't get lost

--- modfiles/data/handlers/migrator.lua
+++ modfiles/data/handlers/migrator.lua
@@ -10,6 +10,7 @@ require("data.migrations.migration_0_17_57")
 require("data.migrations.migration_0_17_61")
 require("data.migrations.migration_0_17_65")
 require("data.migrations.migration_0_18_20")
+require("data.migrations.migration_0_18_27")
 
 -- This code handles the general migration process of the mod's global table
 -- It decides whether and which migrations should be applied, in appropriate order
@@ -28,6 +29,7 @@ migration_masterlist = {
     [10] = {version="0.17.61"},
     [11] = {version="0.17.65"},
     [12] = {version="0.18.20"},
+    [13] = {version="0.18.27"},
 }
 
 -- Applies any appropriate migrations to the global table
