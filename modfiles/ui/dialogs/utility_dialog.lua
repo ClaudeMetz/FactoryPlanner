5eb1cf394484ece15fc3140bef9ecad6db87f522
Claude Metz <claudearmetz@gmail.com>
2020-06-09 22:08:26 +0200

    Add indication of utility items present in inventory

--- modfiles/ui/dialogs/utility_dialog.lua
+++ modfiles/ui/dialogs/utility_dialog.lua
@@ -1,15 +1,20 @@
 -- Handles populating the utility modal dialog
 function open_utility_dialog(flow_modal_dialog)
     flow_modal_dialog.parent.caption = {"fp.utilities"}
-    local context = get_context(game.get_player(flow_modal_dialog.player_index))
 
-    refresh_utility_components_structure(flow_modal_dialog, context)
-    create_utility_notes_structure(flow_modal_dialog, context)
+    local player = game.get_player(flow_modal_dialog.player_index)
+    local modal_data = get_modal_data(player)
+
+    -- Add the players' relevant inventory components to modal_data
+    modal_data.inventory_contents = player.get_main_inventory().get_contents()
+
+    refresh_utility_components_structure(flow_modal_dialog)
+    create_utility_notes_structure(flow_modal_dialog)
 end
 
 
 -- Adds a titlebar for the given type of utility, optionally including a scope switch
-local function add_utility_titlebar(flow, type, tooltip, scope, context)
+local function add_utility_titlebar(flow, type, tooltip, scope, subfactory)
     local flow_titlebar = flow.add{type="flow", name="flow_titlebar", direction="horizontal"}
     flow_titlebar.style.vertical_align = "center"
 
@@ -25,19 +30,24 @@ local function add_utility_titlebar(flow, type, tooltip, scope, context)
         local spacer = flow_titlebar.add{type="flow", direction="horizontal"}
         spacer.style.horizontally_stretchable = true
 
-        local state = Subfactory.get_scope(context.subfactory, type, true)
+        local state = Subfactory.get_scope(subfactory, type, true)
         local switch = flow_titlebar.add{type="switch", name=("fp_switch_utility_scope_" .. type), switch_state=state,
           left_label_caption={"fp.csubfactory"}, right_label_caption={"fp.floor"}}
     end
 end
 
+
 -- Refreshes the flow displaying the appropriate subfactory/floor components
-function refresh_utility_components_structure(flow_modal_dialog, context)
+function refresh_utility_components_structure(flow_modal_dialog)
+    local player = game.get_player(flow_modal_dialog.player_index)
+    local context = get_context(player)
+    local inventory_contents = get_modal_data(player).inventory_contents
+
     local flow = flow_modal_dialog["flow_components"]
     if flow == nil then
         flow = flow_modal_dialog.add{type="flow", name="flow_components", direction="vertical"}
         flow.style.bottom_margin = 12
-        add_utility_titlebar(flow, "components", true, true, context)
+        add_utility_titlebar(flow, "components", true, true, context.subfactory)
         flow.add{type="table", name="table_components", column_count=2}
     end
 
@@ -54,14 +64,24 @@ function refresh_utility_components_structure(flow_modal_dialog, context)
         local table_components = table.add{type="table", column_count=10}
         for _, component in pairs(data) do
             if component.amount > 0 then
-                local singular_name = name:sub(1, -2)
-                local component_text = (component.amount == 1) and {"fp." .. singular_name} or {"fp." .. name}
-                local tooltip = {"", component.proto.localised_name, "\n" ..
-                  ui_util.format_number(component.amount, 4) .. " ", component_text}
+                local button_style = nil
+                local amount_in_inventory = inventory_contents[component.proto.name] or 0
+                if amount_in_inventory == 0 then
+                    button_style = "fp_button_icon_medium_red"
+                elseif amount_in_inventory < component.amount then
+                    button_style = "fp_button_icon_medium_yellow"
+                else
+                    button_style = "fp_button_icon_medium_green"
+                end
 
-                local button = table_components.add{type="sprite-button", name=("sprite-button_" ..
+                local singular_name = name:sub(1, -2)
+                local needed_amount_tt = {("fp.pl_" .. singular_name), component.amount}
+                local tooltip_amounts = {"fp.component_amounts_tt", needed_amount_tt, amount_in_inventory}
+                local tooltip = {"", component.proto.localised_name, "\n", tooltip_amounts}
+
+                table_components.add{type="sprite-button", name=("sprite-button_" ..
                   component.proto.name), sprite=component.proto.sprite, tooltip=tooltip,
-                  style="fp_button_icon_medium_blank", number=component.amount, enabled=false}
+                  style=button_style, number=component.amount, enabled=false}
             end
         end
 
@@ -77,13 +97,15 @@ function refresh_utility_components_structure(flow_modal_dialog, context)
     add_row("modules", data.modules)
 end
 
+
 -- Creates the flow containing this subfactories notes
-function create_utility_notes_structure(flow_modal_dialog, context)
+function create_utility_notes_structure(flow_modal_dialog)
+    local subfactory = get_context(game.get_player(flow_modal_dialog.player_index)).subfactory
+
     local flow = flow_modal_dialog.add{type="flow", name="flow_notes", direction="vertical"}
+    add_utility_titlebar(flow, "notes", false, false, subfactory)
 
-    add_utility_titlebar(flow, "notes", false, false, context)
-
-    local text_box = flow.add{type="text-box", name="fp_text-box_notes", text=context.subfactory.notes}
+    local text_box = flow.add{type="text-box", name="fp_text-box_notes", text=subfactory.notes}
     text_box.style.width = 500
     text_box.style.height = 250
     text_box.word_wrap = true
