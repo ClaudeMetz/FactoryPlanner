c14cd836638f5bcdc684d7d5ee534981b5ce3c4a
Claude Metz <claudearmetz@gmail.com>
2020-06-10 16:23:28 +0200

    Fix subfloor byproducts not properly balancing at the top

--- modfiles/control.lua
+++ modfiles/control.lua
@@ -4,7 +4,7 @@ require("data.init")
 require("ui.listeners")
 
 margin_of_error = 1e-8  -- Margin of error for floating point calculations
---devmode = true  -- Enables certain conveniences for development
+devmode = true  -- Enables certain conveniences for development
 cached_dialogs = {--[[ "fp_frame_modal_dialog_product" ]]}  -- Dialogs that should be cached when closed
 
 if devmode then
