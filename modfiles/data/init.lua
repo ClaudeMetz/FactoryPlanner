34be13a6b44721d28971c796c3a9c34c177f5971
Claude Metz <claudearmetz@gmail.com>
2020-06-17 15:19:58 +0200

    Refactor init file

--- modfiles/data/init.lua
+++ modfiles/data/init.lua
@@ -17,8 +17,103 @@ require("data.handlers.prototyper")
 require("data.handlers.remote")
 require("data.calculation.interface")
 
+init = {}
+
+-- ** LOCAL UTIL **
+-- Reloads the user preferences, incorporating previous preferences if possible
+local function reload_preferences(player)
+    local preferences = global.players[player.index].preferences
+
+    preferences.pause_on_interface = preferences.pause_on_interface or false
+    preferences.tutorial_mode = preferences.tutorial_mode or true
+    preferences.recipe_filters = preferences.recipe_filters or {disabled = false, hidden = false}
+
+    preferences.ignore_barreling_recipes = preferences.ignore_barreling_recipes or false
+    preferences.ignore_recycling_recipes = preferences.ignore_recycling_recipes or false
+    preferences.ingredient_satisfaction = preferences.ingredient_satisfaction or false
+    preferences.round_button_numbers = preferences.round_button_numbers or false
+
+    preferences.optional_production_columns = preferences.optional_production_columns or
+      {["pollution"] = false, ["line_comments"] = false}
+
+    preferences.mb_defaults = preferences.mb_defaults or
+      {module = nil, beacon = nil, beacon_count = nil}
+
+    preferences.default_prototypes = preferences.default_prototypes or {}
+    preferences.default_prototypes = {
+        belts = preferences.default_prototypes.belts or prototyper.defaults.get_fallback("belts"),
+        beacons = preferences.default_prototypes.beacons or prototyper.defaults.get_fallback("beacons"),
+        fuels = preferences.default_prototypes.fuels or prototyper.defaults.get_fallback("fuels"),
+        machines = preferences.default_prototypes.machines or prototyper.defaults.get_fallback("machines")
+    }
+end
+
+-- (Re)sets the UI state of the given player
+local function reset_ui_state(player)
+    -- Delete the whole table first in case ui_state parameter got removed
+    global.players[player.index].ui_state = {}
+    local ui_state_table = global.players[player.index].ui_state
+
+    ui_state_table.main_dialog_dimensions = nil  -- Can only be calculated after on_init
+    ui_state_table.current_activity = nil  -- The current unique main dialog activity
+    ui_state_table.last_action = {}  -- The last user action, used for rate limiting
+    ui_state_table.view_state = nil  -- The state of the production views
+    ui_state_table.message_queue = {}  -- The general message queue
+    ui_state_table.context = ui_util.context.create(player)  -- The currently displayed set of data
+
+    ui_state_table.modal_dialog_type = nil  -- The internal modal dialog type
+    ui_state_table.modal_data = nil  -- Data that can be set for a modal dialog to use
+
+    ui_state_table.flags = {
+        floor_total = false,  -- Whether the floor or subfactory totals are displayed
+        archive_open = false,  -- Wether the players subfactory archive is currently open
+        selection_mode = false  -- Whether the player is currently using a selector
+    }
+end
+
+
+-- Makes sure that the given player has a player_table and a reset gui state
+-- The table attribute specified what table the data should be loaded from (either global or new)
+local function update_player_table(player)
+    local function reload_data()
+        init.reload_settings(player)  -- reloads the settings of the player
+        reload_preferences(player) -- reloads and adjusts the player's preferences
+        reset_ui_state(player)  -- Resets the player's UI state
+    end
+
+    local player_table = global.players[player.index]
+    if player_table == nil then  -- new player
+        global.players[player.index] = {}
+        player_table = global.players[player.index]
+
+        player_table.mod_version = global.mod_version
+        player_table.index = player.index
+
+        player_table.factory = Factory.init()
+        player_table.archive = Factory.init()
+
+        player_table.settings = {}
+        player_table.preferences = {}
+        player_table.ui_state = {}
+        reload_data()
+
+        ui_util.message.enqueue(player, {"fp.hint_tutorial"}, "hint", 5)
+
+    else  -- existing player, only need to update
+        reload_data()
+
+        -- If any subfactories exist, select the first one
+        local subfactories = Factory.get_in_order(player_table.factory, "Subfactory")
+        if #subfactories > 0 then ui_util.context.set_subfactory(player, subfactories[1]) end
+    end
+
+    return player_table
+end
+
+
+
 -- Initiates all factorio-global variables
-function global_init()
+local function global_init()
     global.mod_version = game.active_mods["factoryplanner"]
     global.players = {}
 
@@ -27,13 +122,13 @@ function global_init()
     prototyper.finish()
 
     -- Create player tables for all existing players
-    for index, player in pairs(game.players) do
+    for _, player in pairs(game.players) do
         update_player_table(player)
     end
 end
 
 -- Runs through all updates that need to be made after the config changed
-function handle_configuration_change()
+local function handle_configuration_change()
     prototyper.setup()  -- Setup prototyper
     migrator.attempt_global_migration()  -- Migrate global
 
@@ -68,46 +163,9 @@ function handle_configuration_change()
 end
 
 
--- Makes sure that the given player has a player_table and a reset gui state
--- The table attribute specified what table the data should be loaded from (either global or new)
-function update_player_table(player)
-    local function reload_data()
-        reload_settings(player)  -- reloads the settings of the player
-        reload_preferences(player) -- reloads and adjusts the player's preferences
-        reset_ui_state(player)  -- Resets the player's UI state
-    end
-
-    local player_table = global.players[player.index]
-    if player_table == nil then  -- new player
-        global.players[player.index] = {}
-        local player_table = global.players[player.index]
-
-        player_table.mod_version = global.mod_version
-        player_table.index = player.index
-
-        player_table.factory = Factory.init()
-        player_table.archive = Factory.init()
-
-        player_table.settings = {}
-        player_table.preferences = {}
-        player_table.ui_state = {}
-        reload_data()
-
-        ui_util.message.enqueue(player, {"fp.hint_tutorial"}, "hint", 5)
-
-    else  -- existing player, only need to update
-        reload_data()
-
-        -- If any subfactories exist, select the first one
-        local subfactories = Factory.get_in_order(player_table.factory, "Subfactory")
-        if #subfactories > 0 then ui_util.context.set_subfactory(player, subfactories[1]) end
-    end
-
-    return player_table
-end
-
+-- ** TOP LEVEL **
 -- Writes the current user mod settings to their player_table
-function reload_settings(player)
+function init.reload_settings(player)
     local settings = settings.get_player_settings(player)
     -- Delete the whole table first in case a setting got removed
     global.players[player.index].settings = {}
@@ -122,58 +180,8 @@ function reload_settings(player)
     settings_table.indicate_rounding = tonumber(settings["fp_indicate_rounding"].value)
 end
 
--- Reloads the user preferences, incorporating previous preferences if possible
-function reload_preferences(player)
-    local preferences = global.players[player.index].preferences
 
-    preferences.pause_on_interface = preferences.pause_on_interface or false
-    preferences.tutorial_mode = preferences.tutorial_mode or true
-    preferences.recipe_filters = preferences.recipe_filters or {disabled = false, hidden = false}
-
-    preferences.ignore_barreling_recipes = preferences.ignore_barreling_recipes or false
-    preferences.ignore_recycling_recipes = preferences.ignore_recycling_recipes or false
-    preferences.ingredient_satisfaction = preferences.ingredient_satisfaction or false
-    preferences.round_button_numbers = preferences.round_button_numbers or false
-
-    preferences.optional_production_columns = preferences.optional_production_columns or
-      {["pollution"] = false, ["line_comments"] = false}
-
-    preferences.mb_defaults = preferences.mb_defaults or
-      {module = nil, beacon = nil, beacon_count = nil}
-
-    preferences.default_prototypes = preferences.default_prototypes or {}
-    preferences.default_prototypes = {
-        belts = preferences.default_prototypes.belts or prototyper.defaults.get_fallback("belts"),
-        beacons = preferences.default_prototypes.beacons or prototyper.defaults.get_fallback("beacons"),
-        fuels = preferences.default_prototypes.fuels or prototyper.defaults.get_fallback("fuels"),
-        machines = preferences.default_prototypes.machines or prototyper.defaults.get_fallback("machines")
-    }
-end
-
--- (Re)sets the UI state of the given player
-function reset_ui_state(player)
-    -- Delete the whole table first in case ui_state parameter got removed
-    global.players[player.index].ui_state = {}
-    local ui_state_table = global.players[player.index].ui_state
-
-    ui_state_table.main_dialog_dimensions = nil  -- Can only be calculated after on_init
-    ui_state_table.current_activity = nil  -- The current unique main dialog activity
-    ui_state_table.last_action = {}  -- The last user action, used for rate limiting
-    ui_state_table.view_state = nil  -- The state of the production views
-    ui_state_table.message_queue = {}  -- The general message queue
-    ui_state_table.context = ui_util.context.create(player)  -- The currently displayed set of data
-
-    ui_state_table.modal_dialog_type = nil  -- The internal modal dialog type
-    ui_state_table.modal_data = nil  -- Data that can be set for a modal dialog to use
-
-    ui_state_table.flags = {
-        floor_total = false,  -- Whether the floor or subfactory totals are displayed
-        archive_open = false,  -- Wether the players subfactory archive is currently open
-        selection_mode = false  -- Whether the player is currently using a selector
-    }
-end
-
-
+-- ** EVENTS **
 -- Sets up global data structure of the mod
 script.on_init(global_init)
 
@@ -205,25 +213,7 @@ script.on_event(defines.events.on_player_removed, function(event)
 end)
 
 
--- Updates validity of every class specified by the classes parameter
-function run_validation_updates(parent, classes)
-    local valid = true
-    for type, class in pairs(classes) do
-        if not Collection.update_validity(parent[type], class) then
-            valid = false
-        end
-    end
-    return valid
-end
-
--- Tries to repair every specified class, deletes them if this is unsuccessfull
-function run_invalid_dataset_repair(player, parent, classes)
-    for type, class in pairs(classes) do
-        Collection.repair_invalid_datasets(parent[type], player, class, parent)
-    end
-end
-
-
+-- ** GLOBAL UTIL **
 -- Returns the player table for the given player
 function get_table(player)
     return global.players[player.index]
@@ -251,4 +241,26 @@ end
 
 function get_flags(player)
     return global.players[player.index].ui_state.flags
+end
+
+
+
+
+-- TODO move when classes are re-done
+-- Updates validity of every class specified by the classes parameter
+function run_validation_updates(parent, classes)
+    local valid = true
+    for type, class in pairs(classes) do
+        if not Collection.update_validity(parent[type], class) then
+            valid = false
+        end
+    end
+    return valid
+end
+
+-- Tries to repair every specified class, deletes them if this is unsuccessfull
+function run_invalid_dataset_repair(player, parent, classes)
+    for type, class in pairs(classes) do
+        Collection.repair_invalid_datasets(parent[type], player, class, parent)
+    end
 end
