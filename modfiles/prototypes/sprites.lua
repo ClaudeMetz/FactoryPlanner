ed53357f76de4cd29a26f95c10fe6f10f3bf64e7
Claude Metz <claudearmetz@gmail.com>
2020-07-16 16:04:40 +0200

    Add generic assembler image

--- modfiles/prototypes/sprites.lua
+++ modfiles/prototypes/sprites.lua
@@ -3,29 +3,31 @@ data:extend({
         type = "sprite",
         name = "fp_sprite_plus",
         filename = "__factoryplanner__/graphics/plus.png",
-        priority = "extra-high-no-scale",
-        width = 128,
-        height = 128
+        size = 128
+    },
+    {
+        type = "sprite",
+        name = "fp_generic_assembler",
+        filename = "__factoryplanner__/graphics/generic_assembler.png",
+        size = 62,
+        position = {1, 1}
     },
     {
         type = "sprite",
         name = "fp_sprite_green_arrow_up",
         filename = "__factoryplanner__/graphics/green_arrow_up.png",
-        priority = "extra-high-no-scale",
         size = 40
     },
     {
         type = "sprite",
         name = "fp_sprite_red_arrow_down",
         filename = "__factoryplanner__/graphics/red_arrow_down.png",
-        priority = "extra-high-no-scale",
         size = 40
     },
     {
         type = "sprite",
         name = "fp_sprite_purple_circle",
         filename = "__factoryplanner__/graphics/purple_circle.png",
-        priority = "extra-high-no-scale",
         size = 40
     }
 })
