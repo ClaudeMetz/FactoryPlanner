ac40e0a681636ac00e3863267b63c8ff16fe8d78
Claude Metz <claudearmetz@gmail.com>
2020-07-21 12:40:53 +0200

    Implement crude subfloor unpacking

--- modfiles/data/handlers/builder.lua
+++ modfiles/data/handlers/builder.lua
@@ -107,11 +107,11 @@ function builder.dev_config(player)
 
         -- Subfactories
         local subfactory = Factory.add(factory, Subfactory.init("", {type="item", name="iron-plate"},
-          "one_minute", true))
+          "one_minute"))
         ui_util.context.set_subfactory(player, subfactory)
 
-        Factory.add(factory, Subfactory.init("Beta", nil, "one_minute", true))
-        Factory.add(factory, Subfactory.init("Gamma", {type="item", name="electronic-circuit"}, "one_minute", true))
+        Factory.add(factory, Subfactory.init("Beta", nil, "one_minute"))
+        Factory.add(factory, Subfactory.init("Gamma", {type="item", name="electronic-circuit"}, "one_minute"))
 
         -- Products
         local products = {
@@ -169,7 +169,7 @@ function builder.example_subfactory(player)
 
     -- Always add the example subfactory as a non-archived one
     local subfactory = Factory.add(factory, Subfactory.init("Example",
-      {type="item", name="production-science-pack"}, "one_minute", true))
+      {type="item", name="production-science-pack"}, "one_minute"))
     factory.selected_subfactory = subfactory
     ui_util.context.set_factory(player, factory)
     ui_state.flags.archive_open = false
