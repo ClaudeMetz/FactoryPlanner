9747d90135a9ce7b57e46c409eaca296a10f1445
Claude Metz <claudearmetz@gmail.com>
2020-08-01 00:08:13 +0200

    Redesign the options dialog

--- modfiles/ui/dialogs/generic_dialogs.lua
+++ modfiles/ui/dialogs/generic_dialogs.lua
@@ -46,8 +46,8 @@ function chooser_dialog.open(_, _, modal_data)
       style="inside_shallow_frame_with_padding"}
     content_frame.add{type="label", caption=modal_data.text}
 
-    local frame_choices = content_frame.add{type="frame", direction="horizontal", style="deep_frame_in_shallow_frame"}
-    frame_choices.style.margin = {8, 0, 0, 4}
+    local frame_choices = content_frame.add{type="frame", direction="horizontal", style="slot_button_deep_frame"}
+    frame_choices.style.margin = {10, 0, 4, 0}
     ui_elements.choices_table = frame_choices.add{type="table", column_count=8, style="filter_slot_table"}
 
     for _, definition in ipairs(modal_data.button_definitions) do
@@ -56,69 +56,72 @@ function chooser_dialog.open(_, _, modal_data)
 end
 
 
+
 -- ** OPTIONS **
-local option_creators = {}
-function option_creators.numeric(table, field)
-    local textfield = table.add{type="textfield", name="textfield_option_numeric", text=field.value}
-    textfield.style.left_margin = 1
+local option_creators, option_extractors = {}, {}
+
+function option_creators.numeric_textfield(table, field, ui_elements)
+    local textfield = table.add{type="textfield", name="fp_textfield_options_numeric_" .. field.name, text=field.text}
     textfield.style.width = (field.width or 75)
     ui_util.setup_numeric_textfield(textfield, true, false)
     if field.focus then ui_util.select_all(textfield) end
-
-    local caption = (field.tooltip ~= nil) and {"", field.caption, " [img=info]"} or field.caption
-    local label = table.add{type="label", name="label_option_caption", caption=caption, tooltip=field.tooltip}
-    label.style.font = "fp-font-15p"
-    label.style.left_padding = 2
+    ui_elements["numeric_textfield_" .. field.name] = textfield
 end
 
-function option_creators.on_off_switch(table, field)
-    ui_util.switch.add_on_off(table, field.name, field.value, field.caption, field.tooltip, false)
+function option_extractors.numeric_textfield(element)
+    return tonumber(element.text)
 end
 
 
--- Handles populating the options dialog
-function options_dialog.open(_, flow_modal_dialog, modal_data)
-    flow_modal_dialog.parent.caption = {"", {"fp.set"}, " ", modal_data.title}
-    flow_modal_dialog.add{type="label", name="label_options_text", caption=modal_data.text}
+function option_creators.on_off_switch(table, field, ui_elements)
+    local state = ui_util.switch.convert_to_state(field.state)
+    local switch = table.add{type="switch", name="fp_switch_on_off_options_" .. field.name, switch_state=state,
+      left_label_caption={"fp.on"}, right_label_caption={"fp.off"}}
+      ui_elements["on_off_switch_" .. field.name] = switch
+end
 
-    local flow_options = flow_modal_dialog.add{type="flow", name="flow_options", direction="vertical"}
-    flow_options.style.padding = {6, 4, 10, 10}
-    flow_options.style.vertical_spacing = 10
+function option_extractors.on_off_switch(element)
+    return ui_util.switch.convert_to_boolean(element.switch_state)
+end
+
+
+options_dialog.dialog_settings = (function(modal_data) return {
+    caption = {"fp.two_word_title", {"fp.set"}, modal_data.title}
+} end)
+
+function options_dialog.open(_, _, modal_data)
+    local ui_elements = modal_data.ui_elements
+
+    local content_frame = ui_elements.flow_modal_dialog.add{type="frame", direction="vertical",
+      style="inside_shallow_frame_with_padding"}
+    content_frame.add{type="label", caption=modal_data.text}
+
+    local table_options = content_frame.add{type="table", column_count=2}
+    table_options.style.margin = {12, 0, 4, 8}
+    table_options.style.horizontal_spacing = 24
+    table_options.style.vertical_spacing = 16
 
     for _, field in ipairs(modal_data.fields) do
-        local table_option = flow_options.add{type="table", name="table_option_" .. field.name, column_count=2}
-        table_option.style.horizontal_spacing = 12
+        local caption = (field.tooltip) and {"fp.info_label", field.caption} or field.caption
+        local label = table_options.add{type="label", caption=caption, tooltip=field.tooltip}
+        label.style.font = "heading-3"
 
-        option_creators[field.type](table_option, field)
+        option_creators[field.type](table_options, field, ui_elements)
     end
 end
 
--- Handles closing of the options dialog
-function options_dialog.close(player, action, data)
+function options_dialog.close(player, action, _)
     if action == "submit" then
-        local modal_data = get_ui_state(player).modal_data
-        modal_data.submission_handler(player, modal_data.object, data)
-        main_dialog.refresh(player)
-    end
-end
+        local ui_state = data_util.get("ui_state", player)
+        local modal_data = ui_state.modal_data
 
--- Returns all necessary instructions to create and run conditions on the modal dialog
-function options_dialog.condition_instructions(modal_data)
-    local instructions = {
-        data = {},
-        conditions = nil
-    }
-
-    for _, field in ipairs(modal_data.fields) do
-        if field.type == "numeric" then
-            instructions.data[field.name] = (function(flow_modal_dialog) return tonumber(flow_modal_dialog
-              ["flow_options"]["table_option_" .. field.name]["textfield_option_numeric"].text) end)
-
-        elseif field.type == "on_off_switch" then
-            instructions.data[field.name] = (function(flow_modal_dialog) return ui_util.switch.get_state
-              (flow_modal_dialog["flow_options"]["table_option_" .. field.name], field.name, true) end)
+        local options_data = {}
+        for _, field in pairs(modal_data.fields) do
+            local element = modal_data.ui_elements[field.type .. "_" .. field.name]
+            options_data[field.name] = option_extractors[field.type](element)
         end
-    end
 
-    return instructions
+        modal_data.submission_handler(modal_data.object, options_data)
+        calculation.update(player, ui_state.context.subfactory, true)
+    end
 end
