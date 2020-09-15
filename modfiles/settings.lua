ff63fb34031b8638297494ef0c05618326c3b1cd
Claude Metz <claudearmetz@gmail.com>
2020-09-15 16:04:54 +0200

    Add some more width-notches to settings

--- modfiles/settings.lua
+++ modfiles/settings.lua
@@ -23,7 +23,7 @@ data:extend({
         name = "fp_products_per_row",
         setting_type = "runtime-per-user",
         default_value = 8,
-        allowed_values = {5, 6, 7, 8, 9, 10, 11, 12},
+        allowed_values = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14},
         order = "b"
     },
     {
