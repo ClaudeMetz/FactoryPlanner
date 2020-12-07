f086a48f6cf8a425859a2757a0a5d7a0335c6ade
Claude Metz <claudearmetz@gmail.com>
2020-12-07 21:30:28 +0100

    Fix matrix dialog because I'm a dum-dum

--- modfiles/ui/dialogs/matrix_dialog.lua
+++ modfiles/ui/dialogs/matrix_dialog.lua
@@ -26,8 +26,9 @@ end
 local function create_item_category(modal_data, type, label_arg)
     local flow_category = modal_data.modal_elements.content_frame.add{type="flow", direction="vertical"}
 
-    local label_title = flow_category.add{type="label", caption={"fp.matrix_" .. type .. "_items", label_arg,
-      {"fp.pl_item", label_arg}}}
+    local title_string = (type == "free") and {"fp.matrix_free_items"} or
+      {"fp.matrix_constrained_items", label_arg, {"fp.pl_item", label_arg}}
+    local label_title = flow_category.add{type="label", caption=title_string}
     label_title.style.font = "heading-2"
 
     local frame_items = flow_category.add{type="frame", direction="horizontal", style="slot_button_deep_frame"}
