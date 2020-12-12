666629035b54b62e982c3bf338cd82c27b9632a6
Claude Metz <claudearmetz@gmail.com>
2020-12-12 16:01:52 +0100

    Fix tutorial subfactory import being cached incorrectly

---
+++ modfiles/data/migrations/migration_1_1_6.lua
@@ -0,0 +1,16 @@
+local migration = {}
+
+function migration.global()
+    global.tutorial_subfactory = nil
+end
+
+function migration.player_table(player_table)
+end
+
+function migration.subfactory(subfactory)
+end
+
+function migration.packed_subfactory(packed_subfactory)
+end
+
+return migration
