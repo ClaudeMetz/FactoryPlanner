b2d2020a24be62a5a1535414aeb2b3169cb4e832
curiosity <curiosity-a@users.noreply.github.com>
2021-04-10 14:54:02 +0300

    Implement multiple modules per beacon. Implement multiple modules in the module selection dialog.

--- modfiles/data/handlers/migrator.lua
+++ modfiles/data/handlers/migrator.lua
@@ -23,6 +23,7 @@ local migration_masterlist = {
     [13] = {version="1.1.8", migration=require("data.migrations.migration_1_1_8")},
     [14] = {version="1.1.14", migration=require("data.migrations.migration_1_1_14")},
     [15] = {version="1.1.19", migration=require("data.migrations.migration_1_1_19")},
+    [16] = {version="1.1.20", migration=require("data.migrations.migration_1_1_20")},
 }
 
 -- ** LOCAL UTIL **
