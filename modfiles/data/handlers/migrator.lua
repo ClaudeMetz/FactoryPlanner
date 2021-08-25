b81c73ea12bb9dca86bb233da463cba9b6cf140f
Claude Metz <claudearmetz@gmail.com>
2021-08-25 15:28:38 +0200

    Potential fix for trash crash

--- modfiles/data/handlers/migrator.lua
+++ modfiles/data/handlers/migrator.lua
@@ -24,6 +24,7 @@ local migration_masterlist = {
     [14] = {version="1.1.14", migration=require("data.migrations.migration_1_1_14")},
     [15] = {version="1.1.19", migration=require("data.migrations.migration_1_1_19")},
     [16] = {version="1.1.21", migration=require("data.migrations.migration_1_1_21")},
+    [17] = {version="1.1.25", migration=require("data.migrations.migration_1_1_25")},
 }
 
 -- ** LOCAL UTIL **
