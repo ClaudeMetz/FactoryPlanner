45fab4dc7620b301ad90f5c2d306981f3d62bafa
Claude Metz <claudearmetz@gmail.com>
2021-12-16 14:26:02 +0100

    Move support link to the right of the preferences dialog

--- modfiles/ui/dialogs/preferences_dialog.lua
+++ modfiles/ui/dialogs/preferences_dialog.lua
@@ -240,9 +240,6 @@ function preferences_dialog.open(player, modal_data)
 
     local left_content_frame = add_content_frame()
 
-    local support_frame = left_content_frame.add{type="frame", direction="vertical", style="fp_frame_bordered_stretch"}
-    support_frame.add{type="label", caption={"fp.preferences_support"}}
-
     local bordered_frame = left_content_frame.add{type="frame", direction="vertical", style="fp_frame_bordered_stretch"}
     local label_preferences_info = bordered_frame.add{type="label", caption={"fp.preferences_info"}}
     label_preferences_info.style.single_line = false
@@ -262,6 +259,9 @@ function preferences_dialog.open(player, modal_data)
     preference_structures.prototypes(player, left_content_frame, modal_elements, "wagons")
 
     local right_content_frame = add_content_frame()
+
+    local support_frame = right_content_frame.add{type="frame", direction="vertical", style="fp_frame_bordered_stretch"}
+    support_frame.add{type="label", caption={"fp.preferences_support"}}
 
     preference_structures.prototypes(player, right_content_frame, modal_elements, "fuels")
     preference_structures.prototypes(player, right_content_frame, modal_elements, "machines")
