1f4decd6ec2b2408b86eaa6695686253078f7589
Claude Metz <claudearmetz@gmail.com>
2022-03-21 18:43:33 +0100

    Fix matrix dialog image scaling maybe

--- modfiles/ui/dialogs/matrix_dialog.lua
+++ modfiles/ui/dialogs/matrix_dialog.lua
@@ -29,9 +29,10 @@ local function refresh_item_category(modal_data, type)
     table_items.clear()
 
     for index, proto in ipairs(modal_data[type .. "_items"]) do
-        table_items.add{type="sprite-button", tags={mod="fp", on_gui_click="swap_item_category", type=type, index=index},
-          sprite=proto.sprite, tooltip=proto.localised_name, style="flib_slot_button_default",
+        local button = table_items.add{type="sprite-button", sprite=proto.sprite, tooltip=proto.localised_name,
+          tags={mod="fp", on_gui_click="swap_item_category", type=type, index=index}, style="flib_slot_button_default",
           mouse_button_filter={"left"}}
+        button.style.stretch_image_to_widget_size = true
     end
 end
 
