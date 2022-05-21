88c1ed7415ef678aa844be4477cfb96ea1aa674c
Therenas <claudearmetz@gmail.com>
2022-05-21 15:59:28 +0200

    Fix rate limited search not checking whether dialog is still open

--- modfiles/ui/dialogs/modal_dialog.lua
+++ modfiles/ui/dialogs/modal_dialog.lua
@@ -280,6 +280,8 @@ end
 function NTH_TICK_HANDLERS.run_delayed_modal_search(metadata)
     local player = game.get_player(metadata.player_index)
     local modal_data = data_util.get("modal_data", player)
+    if not modal_data then return end
+
     local searchfield = modal_data.modal_elements.search_textfield
     local search_term = searchfield.text:gsub("^%s*(.-)%s*$", "%1"):lower()
     SEARCH_HANDLERS[modal_data.search_handler_name](player, search_term)
