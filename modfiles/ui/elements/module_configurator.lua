4fcd19645369bd4df9a53fc43caa08c0f84af477
Therenas <claudearmetz@gmail.com>
2022-06-04 17:55:36 +0200

    Fix module config refreshing logic

--- modfiles/ui/elements/module_configurator.lua
+++ modfiles/ui/elements/module_configurator.lua
@@ -2,42 +2,41 @@
 module_configurator = {}
 
 -- ** LOCAL UTIL**
-local function add_module_frame(parent_flow, index, module, module_filters, empty_slots)
+local function determine_slider_config(module, empty_slots)
+    local slider_value = (module) and module.amount or empty_slots
+    local maximum_value = (module) and (module.amount + empty_slots) or empty_slots
+    local minimum_value = (maximum_value == 1) and 0 or 1 -- to make sure that the slider can be created
+    return slider_value, maximum_value, minimum_value
+end
+
+local function add_module_frame(parent_flow,  module, module_filters, empty_slots)
     local module_id = module and module.id or nil
 
     local frame_module = parent_flow.add{type="frame", style="fp_frame_module", direction="horizontal",
-      index=index, tags={module_id=module_id}}
+      tags={module_id=module_id}}
     frame_module.add{type="label", caption={"fp.pu_module", 1}}
 
     local module_name = (module) and module.proto.name or nil
-    local button_module = frame_module.add{type="choose-elem-button", elem_type="item", item=module_name,
-      tags={mod="fp", on_gui_elem_changed="select_module", module_id=module_id}, elem_filters=module_filters,
-      style="fp_sprite-button_inset_tiny"}
+    local button_module = frame_module.add{type="choose-elem-button", name="fp_chooser_module", elem_type="item",
+      item=module_name, tags={mod="fp", on_gui_elem_changed="select_module", module_id=module_id},
+      elem_filters=module_filters, style="fp_sprite-button_inset_tiny"}
     button_module.style.right_margin = 12
 
     frame_module.add{type="label", caption={"fp.amount"}}
 
-    local slider_value = (module) and module.amount or empty_slots
-    local maximum_value = (module) and (module.amount + empty_slots) or empty_slots
-    local minimum_value = maximum_value == 1 and 0 or 1 -- to make sure that the slider can be created
-
-    local slider = frame_module.add{type="slider", name="fp_slider_module_amount", style="fp_slider_module",
-      tags={mod="fp", on_gui_value_changed="module_amount", module_id=module_id},
+    local slider_value, maximum_value, minimum_value = determine_slider_config(module, empty_slots)
+    local slider_style = (maximum_value == 1) and "fp_slider_module_none" or "fp_slider_module"
+    local slider = frame_module.add{type="slider", name="fp_slider_module_amount", style=slider_style,
+      tags={mod="fp", on_gui_value_changed="module_amount", module_id=module_id}, enabled=(maximum_value ~= 1),
       minimum_value=minimum_value, maximum_value=maximum_value, value=slider_value, value_step=0.1}
     -- Fix for the slider value step "not bug" (see https://forums.factorio.com/viewtopic.php?p=516440#p516440)
     -- Fixed by setting step to something other than 1 first, then setting it to 1
     slider.set_slider_value_step(1)
 
-    local textfield = frame_module.add{type="textfield", name="fp_textfield_module_amount",
+    local textfield = frame_module.add{type="textfield", name="fp_textfield_module_amount", enabled=(maximum_value ~= 1),
       text=tostring(slider_value), tags={mod="fp", on_gui_text_changed="module_amount", module_id=module_id}}
     ui_util.setup_numeric_textfield(textfield, false, false)
     textfield.style.width = 40
-
-    if maximum_value == 1 then
-        slider.enabled = false
-        slider.style = "fp_slider_module_none"
-        textfield.enabled = false
-    end
 end
 
 
@@ -59,7 +58,7 @@ local function handle_module_selection(player, tags, event)
         ModuleSet.add(module_set, MODULE_NAME_MAP[new_name], slider.slider_value)
     end
 
-    module_configurator.refresh_modules_flow(player)
+    module_configurator.refresh_modules_flow(player, false)
     -- Sorting and effects refresh is done when the dialog is submitted
 end
 
@@ -72,7 +71,7 @@ local function handle_module_slider_change(player, tags, event)
     if tags.module_id then  -- editing an existing module
         local module = ModuleSet.get(module_set, tags.module_id)
         Module.set_amount(module, new_slider_value)
-        module_configurator.refresh_modules_flow(player, tags.module_id)
+        module_configurator.refresh_modules_flow(player, true)
     else  -- empty line, no influence on anything else
         module_textfield.text = tostring(new_slider_value)
     end
@@ -93,7 +92,7 @@ local function handle_module_textfield_change(player, tags, event)
     if tags.module_id then  -- editing an existing module
         local module = ModuleSet.get(module_set, tags.module_id)
         Module.set_amount(module, new_amount)
-        module_configurator.refresh_modules_flow(player)
+        module_configurator.refresh_modules_flow(player, true)
     else  -- empty line, no influence on anything else
         module_slider.slider_value = new_amount
         event.element.text = tostring(new_amount)
@@ -109,34 +108,50 @@ function module_configurator.add_modules_flow(content_frame, modal_data)
     modal_data.modal_elements["modules_flow"] = flow_modules
 end
 
-function module_configurator.refresh_modules_flow(player, module_id_to_keep)
+function module_configurator.refresh_modules_flow(player, update_only)
     local modal_data = data_util.get("modal_data", player)
     local modules_flow = modal_data.modal_elements.modules_flow
-
-    local index_map = {}
-    -- Selectively skip removing the module flow to keep so the slider can be dragger properly
-    for index, frame in pairs(modules_flow.children) do
-        local module_id = frame.tags.module_id
-        if module_id == nil or module_id ~= module_id_to_keep then
-            index_map[module_id or 0] = index  -- 0 for module_id == nil as it isn't used
-            frame.destroy()
-        end
-    end
 
     local module_filters = ModuleSet.compile_filter(modal_data.module_set)
     local empty_slots = modal_data.module_set.empty_slots
 
-    for _, module in pairs(ModuleSet.get_in_order(modal_data.module_set)) do
-        if module.id ~= module_id_to_keep then
-            add_module_frame(modules_flow, index_map[module.id], module, module_filters, empty_slots)
+    if update_only then
+        -- Update the UI instead of rebuilding it so the slider can be dragger properly
+        for _, frame in pairs(modules_flow.children) do
+            local module_id = frame.tags.module_id
+            if module_id == nil then
+                frame.destroy()  -- destroy empty frame as it'll be re-added below
+            else
+                local module = ModuleSet.get(modal_data.module_set, module_id)
+                if module == nil then
+                    frame.destroy()
+                else
+                    local slider_value, maximum_value, minimum_value = determine_slider_config(module, empty_slots)
+
+                    frame["fp_chooser_module"].elem_value = module.proto.name
+
+                    local textfield = frame["fp_textfield_module_amount"]
+                    textfield.text = tostring(module.amount)
+                    textfield.enabled = (maximum_value ~= 1)
+
+                    local slider = frame["fp_slider_module_amount"]
+                    slider.set_slider_value_step(0.1)  -- bug workaround
+                    slider.set_slider_minimum_maximum(minimum_value, maximum_value)
+                    slider.slider_value = slider_value
+                    slider.set_slider_value_step(1)  -- bug workaround
+                    slider.enabled = (maximum_value ~= 1)
+                    slider.style = (maximum_value == 1) and "fp_slider_module_none" or "fp_slider_module"
+                end
+            end
+        end
+    else
+        modules_flow.clear()
+        for _, module in pairs(ModuleSet.get_in_order(modal_data.module_set)) do
+            add_module_frame(modules_flow, module, module_filters, empty_slots)
         end
     end
 
-    if empty_slots > 0 then
-        local index = #modules_flow.children + 1
-        add_module_frame(modules_flow, index, nil, module_filters, empty_slots)
-    end
-
+    if empty_slots > 0 then add_module_frame(modules_flow, nil, module_filters, empty_slots) end
     if modal_data.submit_checker then modal_data.submit_checker(modal_data) end
 end
 
