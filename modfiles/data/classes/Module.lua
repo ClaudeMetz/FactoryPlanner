8c138f7f8883d8a13a01717407151e216ec42de8
Therenas <claudearmetz@gmail.com>
2022-07-06 22:02:53 +0200

    Fix pasting module on module not limiting module amounts

--- modfiles/data/classes/Module.lua
+++ modfiles/data/classes/Module.lua
@@ -40,6 +40,7 @@ function Module.paste(self, object)
             if ModuleSet.get_by_name(self.parent, object.proto.name) and object.proto.name ~= self.proto.name then
                 return false, "already_exists"
             else
+                object.amount = math.min(object.amount, self.parent.module_limit)
                 ModuleSet.replace(self.parent, self, object)
                 ModuleSet.summarize_effects(self.parent)
                 return true, nil
