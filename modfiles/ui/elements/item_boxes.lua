edb7aaa8151194fa7a50d7e24a84620158ee3d15
Therenas <claudearmetz@gmail.com>
2022-08-24 15:23:07 +0200

    Fix tutorial mode still showing some tooltips

--- modfiles/ui/elements/item_boxes.lua
+++ modfiles/ui/elements/item_boxes.lua
@@ -71,7 +71,8 @@ local function refresh_item_box(player, items, category, subfactory, shows_floor
     local matrix_active = (ui_state.context.subfactory.matrix_free_items ~= nil)
     local limitations = {archive_open = ui_state.flags.archive_open, matrix_active = matrix_active}
     local rb_enabled = (script.active_mods["RecipeBook"] ~= nil)
-    local tutorial_tt = data_util.generate_tutorial_tooltip(action, limitations, rb_enabled)
+    local tutorial_tt = (data_util.get("preferences", player).tutorial_mode) and
+      data_util.generate_tutorial_tooltip(action, limitations, rb_enabled) or nil
 
     for _, item in ipairs(items) do
         local required_amount = (not shows_floor_items and category == "product") and Item.required_amount(item) or nil
