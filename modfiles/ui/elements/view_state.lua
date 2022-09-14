5d7d09cd766bc89ac1526d44594c59d64c4b32ff
Therenas <claudearmetz@gmail.com>
2022-09-14 15:32:48 +0200

    Fix crash with fourth view state

--- modfiles/ui/elements/view_state.lua
+++ modfiles/ui/elements/view_state.lua
@@ -66,7 +66,7 @@ end
 function processors.items_per_second_per_machine(metadata, raw_amount, item_proto, machine_count)
     if machine_count == 0 then return 0, "" end  -- avoid division by zero
 
-    local raw_number = raw_amount * metadata.timescale_inverse / (math.ceil((machine_count - 0.001) or 1))
+    local raw_number = raw_amount * metadata.timescale_inverse / (math.ceil((machine_count or 1) - 0.001))
     local number = ui_util.format_number(raw_number, metadata.formatting_precision)
 
     local plural_parameter = (number == "1") and 1 or 2
