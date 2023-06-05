9abe773e5063e2a41be3f40a2052eacf5d299ebe
Therenas <claudearmetz@gmail.com>
2023-06-05 11:16:29 +0200

    Integrate mod_gui with gui util file

--- modfiles/ui/base/main_dialog.lua
+++ modfiles/ui/base/main_dialog.lua
@@ -172,7 +172,7 @@ listeners.gui = {
             handler = (function(player, _, _)
                 if DEV_ACTIVE then  -- implicit mod reload for easier development
                     util.gui.reset_player(player)  -- destroys all FP GUIs
-                    util.mod_gui.toggle(player)  -- fixes the mod gui button after its been destroyed
+                    util.gui.toggle_mod_gui(player)  -- fixes the mod gui button after its been destroyed
                     game.reload_mods()  -- toggle needs to be delayed by a tick since the reload is not instant
                     game.print("Mods reloaded")
                     util.nth_tick.register((game.tick + 1), "interface_toggle", {player_index=player.index})
--- modfiles/util/util.lua
+++ modfiles/util/util.lua
@@ -4,7 +4,6 @@ local _util = {
     clipboard = require("util.clipboard"),
     messages = require("util.messages"),
     raise = require("util.raise"),
-    mod_gui = require("util.mod_gui"),
     cursor = require("util.cursor"),
     gui = require("util.gui"),
     format = require("util.format"),
