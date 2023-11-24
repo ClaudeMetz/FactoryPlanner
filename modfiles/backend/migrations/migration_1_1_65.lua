5aca98a2f2d661b8b0409fabb90853f482be37ca
Francois Campbell <francoiscampbell@users.noreply.github.com>
2023-11-24 10:21:43 -0500

    Fix dataset iteration in migration 1.1.65

--- modfiles/backend/migrations/migration_1_1_65.lua
+++ modfiles/backend/migrations/migration_1_1_65.lua
@@ -4,7 +4,7 @@ local migration = {}
 
 function migration.player_table(player_table)
     for _, factory in pairs({"factory", "archive"}) do
-        for subfactory in pairs(player_table[factory].Subfactory.datasets) do
+        for _, subfactory in pairs(player_table[factory].Subfactory.datasets) do
             subfactory.blueprints = {}
         end
     end
