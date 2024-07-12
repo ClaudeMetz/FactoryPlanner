dc110f091e80f0261f6df0a129695d8a3df5eae7
Therenas <claudearmetz@gmail.com>
2024-07-12 12:07:11 +0200

    Update cursor combinator methods for new combinator

--- modfiles/util/cursor.lua
+++ modfiles/util/cursor.lua
@@ -7,8 +7,8 @@ local function set_cursor_blueprint(player, blueprint_entities)
     local blank_slot = script_inventory[1]
 
     blank_slot.set_stack{name="blueprint"}
-    -- TODO This is somehow broken, probably an API bug
     blank_slot.set_blueprint_entities(blueprint_entities)
+    player.clear_cursor()
     player.add_to_clipboard(blank_slot)
     player.activate_paste()
     script_inventory.destroy()
@@ -38,8 +38,8 @@ function _cursor.set_entity(player, line, object)
         local inventory_list = {}
         for i = 1, module.amount do
             table.insert(inventory_list, {
-                inventory = 4,  -- this should be from defines.inventory depending on the entity,
-                                -- but it seemingly works this way. Not sure.
+                -- This should be from defines.inventory depending on the entity, but this somehow works
+                inventory = 4,
                 stack = slot_index
             })
             slot_index = slot_index + 1
@@ -68,76 +68,88 @@ function _cursor.set_entity(player, line, object)
 end
 
 ---@param player LuaPlayer
----@param item_signals { [SignalID]: number }
+---@param item_signals Signal[]
 function _cursor.set_item_combinator(player, item_signals)
-    local combinator_proto = game.entity_prototypes["constant-combinator"]
-    local filter_limit = combinator_proto.item_slot_count
-
-    local blueprint_entities = {}  ---@type BlueprintEntity[]
-    local current_combinator, current_filter_count = nil, 0
-    local next_entity_number, next_position = 1, {0, 0}
-
-    for signal, amount in pairs(item_signals) do
-        if not current_combinator or current_filter_count == filter_limit then
-            current_combinator = {
-                entity_number = next_entity_number,
-                name = "constant-combinator",
-                tags = {fp_item_combinator = true},
-                position = next_position,
-                control_behavior = {filters = {}},
-                connections = {{green = {}}}  -- filled in below
-            }
-            table.insert(blueprint_entities, current_combinator)
-
-            next_entity_number = next_entity_number + 1
-            next_position = {next_position[1] + 1, 0}
-            current_filter_count = 0
-        end
-
-        current_filter_count = current_filter_count + 1
-        table.insert(current_combinator.control_behavior.filters, {
-            signal = signal,
-            count = math.max(amount, 1),  -- make sure amounts < 1 are not excluded
-            index = current_filter_count
+    local filters, slot_index = {}, 1
+    for _, signal in pairs(item_signals) do
+        table.insert(filters, {
+            index = slot_index,
+            type = signal.signal.type,
+            name = signal.signal.name,
+            count = math.max(signal.count, 1)  -- make sure amounts < 1 are not excluded
         })
+        slot_index = slot_index + 1
     end
 
-    ---@param main_entity BlueprintEntity
-    ---@param other_entity BlueprintEntity
-    local function connect_if_entity_exists(main_entity, other_entity)
-        if other_entity ~= nil then
-            local entry = {entity_id = other_entity.entity_number}
-            table.insert(main_entity.connections[1].green, entry)
-        end
-    end
+    local blueprint_entity = {
+        entity_number = 1,
+        name = "constant-combinator",
+        position = {0, 0},
+        control_behavior = {
+            -- TODO outer 'sections' will be renamed to logistic_sections
+            sections = { sections = {
+                {
+                    index = 1,
+                    filters = filters
+                }
+            } }
+        }
+    }
 
-    for index, entity in ipairs(blueprint_entities) do
-        connect_if_entity_exists(entity, blueprint_entities[index-1])
-        if not next(entity.connections[1].green) then entity.connections = nil end
-    end
-
-    set_cursor_blueprint(player, blueprint_entities)
+    set_cursor_blueprint(player, {blueprint_entity})
 end
 
 ---@param player LuaPlayer
 ---@param proto FPItemPrototype | FPFuelPrototype
 ---@param amount number
 function _cursor.add_to_item_combinator(player, proto, amount)
-    local blueprint_entities = player.get_blueprint_entities()
-    local item_signals = {}
+    local item_signals, filter_matched = {}, false
 
-    if blueprint_entities ~= nil then
-        for _, entity in pairs(blueprint_entities) do
-            if entity.tags ~= nil and entity.tags["fp_item_combinator"] then
-                for _, filter in pairs(entity.control_behavior.filters) do
-                    item_signals[filter.signal] = filter.count
+    local function parse_filters(filters)
+        for _, filter in pairs(filters) do
+            local count = filter.count
+            if proto.type == (filter.type or "item") and proto.name == filter.name then
+                filter_matched = true
+                count = count + amount
+            end
+
+            table.insert(item_signals, {
+                signal = {
+                    type = filter.type,
+                    name = filter.name,
+                },
+                count = count
+            })
+        end
+    end
+
+    if not player.is_cursor_empty() then
+        local cursor = player.cursor_stack  --[[@cast cursor -nil]]
+        if cursor.is_blueprint and cursor.is_blueprint_setup() then
+            local entities = cursor.get_blueprint_entities()
+            if entities and #entities == 1 and entities[1].name == "constant-combinator" then
+                -- TODO outer 'sections' will be renamed to logistic_sections
+                local sections = entities[1].control_behavior.sections
+                if sections and sections.sections and #sections.sections == 1 then
+                    local section = sections.sections[1]
+                    if not section.group then
+                        parse_filters(section.filters)
+                    end
                 end
             end
         end
     end
 
-    local signal = {type=proto.type, name=proto.name}
-    item_signals[signal] = (item_signals[signal] or 0) + amount  -- add to existing if applicable
+    if not filter_matched then
+        table.insert(item_signals, {
+            signal = {
+                type = proto.type,
+                name = proto.name,
+            },
+            count = amount
+        })
+    end
+
     _cursor.set_item_combinator(player, item_signals)
 end
 
