655370da40b0dc29aa883852f7bb49f5823ac27b
Therenas <claudearmetz@gmail.com>
2024-07-27 21:31:22 +0200

    Move production bar to the top

--- modfiles/ui/main/production_box.lua
+++ modfiles/ui/main/production_box.lua
@@ -2,14 +2,6 @@ local Line = require("backend.data.Line")
 local matrix_engine = require("backend.calculation.matrix_engine")
 
 -- ** LOCAL UTIL **
-local function refresh_production(player, _, _)
-    local factory = util.context.get(player, "Factory")
-    if factory and factory.valid then
-        solver.update(player, factory)
-        util.raise.refresh(player, "factory", nil)
-    end
-end
-
 local function refresh_paste_button(player)
     local main_elements = util.globals.main_elements(player)
     if not main_elements.production_box then return end
@@ -97,13 +89,6 @@ local function paste_line(player, _, _)
     util.clipboard.dummy_paste(player, dummy_line, floor)
 end
 
-local function change_floor(player, destination)
-    if util.context.ascend_floors(player, destination) then
-        -- Only refresh if the floor was indeed changed
-        util.raise.refresh(player, "production", nil)
-    end
-end
-
 local function switch_matrix_item(player, tags, event)
     local factory = util.context.get(player, "Factory")  --[[@as Factory]]
 
@@ -137,24 +122,7 @@ local function refresh_production_box(player)
     if not visible then return end
 
     local factory_valid = factory ~= nil and factory.valid
-    local current_level = (factory_valid) and floor.level or 1
     local any_lines_present = factory_valid and not factory.archived and floor:count() > 0
-
-    production_box_elements.refresh_button.enabled = any_lines_present
-    production_box_elements.level_label.caption = (not factory_valid) and ""
-        or {"fp.bold_label", {"", {"fp.level"}, " ", current_level}}
-
-    production_box_elements.floor_up_button.visible = factory_valid
-    production_box_elements.floor_up_button.enabled = (current_level > 1)
-
-    production_box_elements.floor_top_button.visible = factory_valid
-    production_box_elements.floor_top_button.enabled = (current_level > 1)
-
-    production_box_elements.separator_line.visible = factory_valid
-    production_box_elements.utility_dialog_button.visible = factory_valid
-
-    util.raise.refresh(player, "view_state", production_box_elements.view_state_table)
-    production_box_elements.view_state_table.visible = factory_valid
 
     production_box_elements.instruction_label.visible = false
     if factory == nil then
@@ -186,49 +154,6 @@ local function build_production_box(player)
     local frame_vertical = parent_flow.add{type="frame", direction="vertical", style="inside_deep_frame"}
     main_elements.production_box["vertical_frame"] = frame_vertical
 
-    local subheader = frame_vertical.add{type="frame", direction="horizontal", style="subheader_frame"}
-    subheader.style.maximal_height = 100  -- large value to nullify maximal_height
-    subheader.style.padding = {8, 8, 6, 8}
-
-    local button_refresh = subheader.add{type="sprite-button", tags={mod="fp", on_gui_click="refresh_production"},
-        sprite="utility/refresh", style="tool_button", tooltip={"fp.refresh_production"}, mouse_button_filter={"left"}}
-    main_elements.production_box["refresh_button"] = button_refresh
-
-    local label_title = subheader.add{type="label", caption={"fp.production"}, style="frame_title"}
-    label_title.style.padding = {0, 8}
-
-    local label_level = subheader.add{type="label"}
-    label_level.style.right_margin = 8
-    main_elements.production_box["level_label"] = label_level
-
-    local button_floor_up = subheader.add{type="sprite-button", sprite="fp_arrow_line_up",
-        tooltip={"fp.floor_up_tt"}, tags={mod="fp", on_gui_click="change_floor", destination="up"},
-        style="fp_sprite-button_rounded_icon", mouse_button_filter={"left"}}
-    main_elements.production_box["floor_up_button"] = button_floor_up
-
-    local button_floor_top = subheader.add{type="sprite-button", sprite="fp_arrow_line_bar_up",
-        tooltip={"fp.floor_top_tt"}, tags={mod="fp", on_gui_click="change_floor", destination="top"},
-        style="fp_sprite-button_rounded_icon", mouse_button_filter={"left"}}
-    button_floor_top.style.padding = {3, 2, 1, 2}
-    main_elements.production_box["floor_top_button"] = button_floor_top
-
-    local separator = subheader.add{type="line", direction="vertical"}
-    separator.style.margin = {0, 8}
-    main_elements.production_box["separator_line"] = separator
-
-    local button_utility_dialog = subheader.add{type="button", caption={"fp.utilities"},
-        tooltip={"fp.utility_dialog_tt"}, tags={mod="fp", on_gui_click="open_utility_dialog"},
-        style="rounded_button", mouse_button_filter={"left"}}
-    button_utility_dialog.style.minimal_width = 0
-    button_utility_dialog.style.height = 26
-    main_elements.production_box["utility_dialog_button"] = button_utility_dialog
-
-    subheader.add{type="empty-widget", style="flib_horizontal_pusher"}
-
-    util.raise.build(player, "view_state", subheader)
-    main_elements.production_box["view_state_table"] = subheader["table_view_state"]
-
-
     local scroll_pane_production = frame_vertical.add{type="scroll-pane", style="flib_naked_scroll_pane_no_padding"}
     scroll_pane_production.style.extra_right_padding_when_activated = -12
     main_elements.production_box["production_scroll_pane"] = scroll_pane_production
@@ -244,6 +169,7 @@ local function build_production_box(player)
     main_elements.production_box["paste_button"] = button_paste
 
     frame_vertical.add{type="empty-widget", style="flib_vertical_pusher"}
+    frame_vertical.add{type="empty-widget", style="flib_horizontal_pusher"}
 
     local scroll_pane_messages = frame_vertical.add{type="scroll-pane", vertical_scroll_policy="never",
         visible=false, style="flib_naked_scroll_pane_no_padding"}
@@ -279,33 +205,6 @@ local listeners = {}
 listeners.gui = {
     on_gui_click = {
         {
-            name = "refresh_production",
-            timeout = 20,
-            handler = (function(player, _, _)
-                if DEV_ACTIVE then  -- implicit mod reload for easier development
-                    util.gui.reset_player(player)  -- destroys all FP GUIs
-                    util.gui.toggle_mod_gui(player)  -- fixes the mod gui button after its been destroyed
-                    game.reload_mods()  -- toggle needs to be delayed by a tick since the reload is not instant
-                    game.print("Mods reloaded")
-                    util.nth_tick.register((game.tick + 1), "interface_toggle", {player_index=player.index})
-                else
-                    refresh_production(player, nil, nil)
-                end
-            end)
-        },
-        {
-            name = "change_floor",
-            handler = (function(player, tags, _)
-                change_floor(player, tags.destination)
-            end)
-        },
-        {
-            name = "open_utility_dialog",
-            handler = (function(player, _, _)
-                util.raise.open_dialog(player, {dialog="utility"})
-            end)
-        },
-        {
             name = "paste_line",
             handler = paste_line
         },
@@ -317,16 +216,6 @@ listeners.gui = {
 }
 
 listeners.misc = {
-    fp_refresh_production = (function(player, _, _)
-        if main_dialog.is_in_focus(player) then refresh_production(player, nil, nil) end
-    end),
-    fp_up_floor = (function(player, _, _)
-        if main_dialog.is_in_focus(player) then change_floor(player, "up") end
-    end),
-    fp_top_floor = (function(player, _, _)
-        if main_dialog.is_in_focus(player) then change_floor(player, "top") end
-    end),
-
     build_gui_element = (function(player, event)
         if event.trigger == "main_dialog" then
             build_production_box(player)
