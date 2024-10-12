08895d3b57790d05442cf917b9f2ff707c792eb3
Therenas <claudearmetz@gmail.com>
2024-10-12 12:07:50 +0200

    Fix crash when changing dialog dimensions while paused

--- modfiles/ui/base/main_dialog.lua
+++ modfiles/ui/base/main_dialog.lua
@@ -142,6 +142,7 @@ end
 function main_dialog.set_pause_state(player, frame_main_dialog, force_false)
     -- Don't touch paused-state if this is a multiplayer session or the editor is active
     if game.is_multiplayer() or player.controller_type == defines.controllers.editor then return end
+    if not frame_main_dialog or not frame_main_dialog.valid then return end
 
     game.tick_paused = (util.globals.preferences(player).pause_on_interface and not force_false)
         and frame_main_dialog.visible or false
