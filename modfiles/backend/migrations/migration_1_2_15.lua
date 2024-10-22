cad55c9a3f822a08be92ccc85b16788cdefbbea6
Therenas <claudearmetz@gmail.com>
2024-10-22 12:11:24 +0200

    Fix crash on migration

--- modfiles/backend/migrations/migration_1_2_15.lua
+++ modfiles/backend/migrations/migration_1_2_15.lua
@@ -6,8 +6,11 @@ function migration.global()
 end
 
 function migration.player_table(player_table)
+    local item_views = player_table.preferences.item_views
+    if item_views == nil then return end
+
     local preferences = { views = {}, selected_index = nil }
-    for index, view in pairs(player_table.preferences.item_views) do
+    for index, view in pairs(item_views) do
         if view.selected then preferences.selected_index = index end
         table.insert(preferences.views, {name=view.name, enabled=view.enabled})
     end
