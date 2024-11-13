418bc1efc91a25a6f0ee0e096922fadff9f4f8dc
Therenas <claudearmetz@gmail.com>
2024-11-13 18:15:48 +0100

    Diff District products and ingredients against each other for display

--- modfiles/backend/data/DistrictItem.lua
+++ modfiles/backend/data/DistrictItem.lua
@@ -4,6 +4,7 @@ local Object = require("backend.data.Object")
 ---@field class "DistrictItem"
 ---@field proto FPItemPrototype
 ---@field amount number
+---@field converse_amount number
 local DistrictItem = Object.methods()
 DistrictItem.__index = DistrictItem
 script.register_metatable("DistrictItem", DistrictItem)
@@ -14,7 +15,8 @@ script.register_metatable("DistrictItem", DistrictItem)
 local function init(proto, amount)
     local object = Object.init({
         proto = proto,
-        amount = amount
+        amount = amount,  -- amount filled in by the factories
+        converse_amount = 0  -- amount of this item in converse category
     }, "DistrictItem", DistrictItem)  --[[@as DistrictItem]]
     return object
 end
