786a9ed2fa7d8c39017f75ecca3c836859e3df54
Therenas <claudearmetz@gmail.com>
2024-11-14 11:03:40 +0100

    Disable machine limit GUI instead of hiding it

--- modfiles/ui/dialogs/machine_dialog.lua
+++ modfiles/ui/dialogs/machine_dialog.lua
@@ -120,7 +120,7 @@ local function reset_machine(player)
     modal_elements["machine_button"].elem_value = machine:elem_value()
 
     local limit_switch = modal_elements.force_limit_switch
-    if limit_switch ~= nil then
+    if limit_switch.enabled then
         modal_elements["limit_textfield"].text = machine.limit or ""
         limit_switch.switch_state = util.gui.switch.convert_to_state(machine.force_limit)
     end
@@ -171,7 +171,7 @@ local function add_fuel_frame(parent_frame, player, line)
 end
 
 
-local function add_limit_frame(parent_frame, player)
+local function add_limit_frame(parent_frame, player, enabled)
     local modal_data = util.globals.modal_data(player)  --[[@as table]]
     local machine = modal_data.object
 
@@ -182,7 +182,7 @@ local function add_limit_frame(parent_frame, player)
     local textfield_width = 45
     local textfield_limit = frame_limit.add{type="textfield", tags={mod="fp", on_gui_text_changed="machine_limit",
         on_gui_confirmed="confirm_machine", width=textfield_width}, tooltip={"fp.expression_textfield"},
-        text=machine.limit}
+        text=machine.limit, enabled=enabled}
     textfield_limit.style.width = textfield_width
     modal_data.modal_elements["limit_textfield"] = textfield_limit
 
@@ -192,7 +192,13 @@ local function add_limit_frame(parent_frame, player)
 
     local state = util.gui.switch.convert_to_state(machine.force_limit)
     local switch_force_limit = util.gui.switch.add_on_off(frame_limit, nil, {}, state)
+    switch_force_limit.enabled = enabled
     modal_data.modal_elements["force_limit_switch"] = switch_force_limit
+
+    if not enabled then
+        frame_limit.add{type="label", caption={"fp.machine_limit_unavailable"},
+            tooltip={"fp.machine_limit_unavailable_tt"}}
+    end
 end
 
 
@@ -259,9 +265,8 @@ local function open_machine_dialog(player, modal_data)
     -- Limit
     local factory = util.context.get(player, "Factory")
     -- Unavailable with matrix solver or special recipes
-    if factory.matrix_free_items == nil and modal_data.line.recipe_proto.energy > 0 then
-        add_limit_frame(content_frame, player)
-    end
+    local limit_enabled = (factory.matrix_free_items == nil and modal_data.line.recipe_proto.energy > 0)
+    add_limit_frame(content_frame, player, limit_enabled)
 
     -- Modules
     module_configurator.add_modules_flow(content_frame, modal_data)
@@ -280,7 +285,7 @@ local function close_machine_dialog(player, action)
         machine.module_set:normalize({sort=true})
 
         local limit_switch = modal_data.modal_elements.force_limit_switch
-        if limit_switch ~= nil then
+        if limit_switch.enabled then
             machine.limit = util.gui.parse_expression_field(modal_data.modal_elements.limit_textfield)
             machine.force_limit = util.gui.switch.convert_to_boolean(limit_switch.switch_state)
         end
