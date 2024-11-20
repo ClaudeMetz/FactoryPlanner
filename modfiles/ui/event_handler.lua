eef86451ef0fe2005bce5d9977b8c86ae6028f29
Therenas <claudearmetz@gmail.com>
2024-11-20 23:33:10 +0100

    Fix context menu not quite always closing when clicking elsewhere

--- modfiles/ui/event_handler.lua
+++ modfiles/ui/event_handler.lua
@@ -121,21 +121,25 @@ end
 local function handle_gui_event(event)
     if not event.element then return end
 
-    local tags = event.element.tags
-    if tags.mod ~= "fp" then return end
+    -- GUI events always have an associated player
+    local player = game.get_player(event.player_index)  ---@cast player -nil
 
     -- Guard against an event being called before the player is initialized
     if not storage.players[event.player_index] then return end
 
-    -- GUI events always have an associated player
-    local player = game.get_player(event.player_index)  ---@cast player -nil
+    local tags = event.element.tags
+
+    -- Close an open context menu on any GUI click
+    if event.name == defines.events.on_gui_click and
+            not tags.on_gui_click ~= "choose_context_action" then
+        modal_dialog.close_context_menu(player)
+    end
+
+    if tags.mod ~= "fp" then return end
 
     -- The event table actually contains its identifier, not its name
     local event_name = gui_identifier_map[event.name]
     local action_name = tags[event_name]  -- could be nil
-
-    -- Close an open context menu on any GUI click
-    if event_name == "on_gui_click" then modal_dialog.close_context_menu(player) end
 
     -- If a special handler is set, it needs to return true before proceeding with the registered handlers
     local special_handler = special_gui_handlers[event_name]
