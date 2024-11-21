9582b1616bbe296807cf9993212042d7818b6039
Therenas <claudearmetz@gmail.com>
2024-11-22 00:36:07 +0100

    Update shortcut icon to new size an fix its color

--- modfiles/prototypes/shortcuts.lua
+++ modfiles/prototypes/shortcuts.lua
@@ -8,8 +8,8 @@ data:extend({
         toggleable = false,
         order = "fp-a[open]",
         associated_control_input = "fp_toggle_interface",
-        icon = "__factoryplanner__/graphics/shortcut_open_x32.png",
-        icon_size = 32,
+        icon = "__factoryplanner__/graphics/shortcut_open_x56.png",
+        icon_size = 56,
         small_icon = "__factoryplanner__/graphics/shortcut_open_x24.png",
         small_icon_size = 24
     }
