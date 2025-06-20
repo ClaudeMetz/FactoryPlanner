37094a7f2a93c15831b54c89ead90cd87c004b40
Therenas <claudearmetz@gmail.com>
2025-06-20 15:59:47 +0200

    Fix migration issue from pre-pumps

--- modfiles/backend/migrations/migration_2_0_21.lua
+++ modfiles/backend/migrations/migration_2_0_21.lua
@@ -3,7 +3,9 @@
 local migration = {}
 
 function migration.player_table(player_table)
-    player_table.preferences.default_pumps.quality = "normal"
+    if player_table.preferences.default_pumps then
+        player_table.preferences.default_pumps.quality = "normal"
+    end
     player_table.preferences.default_wagons[1].quality = "normal"
     player_table.preferences.default_wagons[2].quality = "normal"
 end
