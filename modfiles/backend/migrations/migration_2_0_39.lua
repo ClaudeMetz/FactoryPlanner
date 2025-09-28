728dac1c9a6621b281a836c9a9ea3753b9fa84f2
Therenas <claudearmetz@gmail.com>
2025-09-28 10:19:23 +0200

    Add migration to fix crash on saves created on 2.0.37

---
+++ modfiles/backend/migrations/migration_2_0_39.lua
@@ -0,0 +1,24 @@
+---@diagnostic disable
+
+local migration = {}
+
+local function check_quality(default)
+    default.quality = default.quality or PROTOTYPE_MAPS.qualities["normal"]
+    if default.modules then
+        for _, module in pairs(default.modules) do
+            check_quality(module)
+        end
+    end
+end
+
+function migration.player_table(player_table)
+    for _, machine in pairs(player_table.preferences.default_machines) do
+        check_quality(machine)
+    end
+    check_quality(player_table.preferences.default_beacons)
+    check_quality(player_table.preferences.default_pumps)
+    check_quality(player_table.preferences.default_wagons[1])
+    check_quality(player_table.preferences.default_wagons[2])
+end
+
+return migration
