235e37b3b639785981ceccb4d2d23a92d266a5c1
Therenas <claudearmetz@gmail.com>
2026-06-30 18:49:41 +0200

    Fix migration issue due to Product object rename

--- modfiles/backend/migrations/migration_2_1_4.lua
+++ modfiles/backend/migrations/migration_2_1_4.lua
@@ -1,12 +1,19 @@
 ---@diagnostic disable
 
+local TLProduct = require("backend.data.TLProduct")
+
 local migration = {}
 
 function migration.player_table(player_table)
     for district in player_table.realm:iterator() do
         for factory in district:iterator() do
             for _, product in pairs(factory:as_list()) do
-                product.class = "TLProduct"
+                -- Need to rebuild so the metatables work out
+                local new_product = TLProduct.init(product.proto)
+                new_product.defined_by = product.defined_by
+                new_product.required_amount = product.required_amount
+                new_product.belt_proto = product.belt_proto
+                factory:replace(product, new_product)
             end
         end
     end
