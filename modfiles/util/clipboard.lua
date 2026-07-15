31712c416a948e8313192173133c13ab9d366da1
Vlad-Andrei Popescu <vladandreipopescu97@gmail.com>
2026-07-15 19:09:04 +0300

    Add error on fluid paste on top-level with unset temperature (#769)
    * Add error on fluid paste on top-level with unset temperature
    
    * Merge branch 'master' of github.com:ClaudeMetz/FactoryPlanner into fix_top_level_fluid_paste
    
    * Fix typing
    
    * Change to floating error
    
    * Adjust locale

--- modfiles/util/clipboard.lua
+++ modfiles/util/clipboard.lua
@@ -74,6 +74,8 @@ function _clipboard.paste(player, target)
                 lib.cursor.create_flying_text(player, {"fp.clipboard_no_empty_slots"})
             elseif error == "recipe_irrelevant" then
                 lib.cursor.create_flying_text(player, {"fp.clipboard_recipe_irrelevant"})
+            elseif error == "temperature_not_set" then
+                lib.cursor.create_flying_text(player, {"fp.clipboard_temperature_not_set"})
             end
         end
         return success
