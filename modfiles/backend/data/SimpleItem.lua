d831b72dcd834acd58ab67f9c65c957ad8b3a78a
Therenas <claudearmetz@gmail.com>
2026-07-31 17:40:44 +0200

    Minor adjustements

--- modfiles/backend/data/SimpleItem.lua
+++ modfiles/backend/data/SimpleItem.lua
@@ -1,4 +1,5 @@
----@class SimpleItem not an `Object`
+--- Not an real Object, but has a metatable
+---@class SimpleItem
 ---@field class "SimpleItem"
 ---@field proto FPItemPrototype
 ---@field amount number
@@ -7,22 +8,20 @@
 local SimpleItem = {}
 SimpleItem.__index = SimpleItem
 
-
 ---@param parent LineObject?
 ---@param proto FPItemPrototype
 ---@param amount number?
 ---@return SimpleItem
 function SimpleItem:init(parent, proto, amount)
-    ---@diagnostic disable-next-line: missing-fields
-    local o = {
+    local item = {
         proto = proto,
         class = "SimpleItem",
         amount = amount or 0,
         satisfied_amount = nil,
         parent = parent  -- can be nil
     }  ---@type SimpleItem
-    setmetatable(o, self)
-    return o
+    setmetatable(item, self)
+    return item
 end
 
 
@@ -31,20 +30,17 @@ end
 ---@return string? error
 function SimpleItem:paste(object)
     if object.class == "SimpleItem" or object.class == "Fuel" then
-        ---@diagnostic disable-next-line: cast-type-mismatch
         ---@cast object SimpleItem | Fuel
 
         -- Only pasting on a line item is allowed
         if not self.parent or self.parent.class ~= "Line" then
             return false, "incompatible"
         end
-
         -- Only pasting on fluids is allowed
         if object.proto.type ~= "fluid" or self.proto.type ~= "fluid" then
             return false, "incompatible"
         end
 
-        -- SimpleItems will always be a fluid with temperature
         if object.class == "SimpleItem" then  ---@cast object SimpleItem
             if object.proto.base_name ~= self.proto.name then return false, "incompatible" end
             if not object.proto.temperature then return false, "incompatible" end
@@ -66,6 +62,5 @@ function SimpleItem:paste(object)
         return false, "incompatible_class"
     end
 end
-
 
 return SimpleItem
