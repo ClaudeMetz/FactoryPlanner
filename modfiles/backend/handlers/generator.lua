43429485a0da1616a566d7ec442ca4c55cc2d2f3
Therenas <claudearmetz@gmail.com>
2026-08-01 22:19:14 +0200

    Fix boiler heat capactity with output filter and an unfiltered input

--- modfiles/backend/handlers/generator.lua
+++ modfiles/backend/handlers/generator.lua
@@ -372,7 +372,7 @@ function generator.recipes.generate()
                 local product_name, product_amount = fluid_proto.name, 1.0
                 if output ~= nil and output.filter ~= nil then
                     product_name = output.filter.name
-                    product_amount = 1 * (input.filter--[[@cast -nil]].heat_capacity / output.filter.heat_capacity)
+                    product_amount = 1 * (fluid_proto.heat_capacity / output.filter.heat_capacity)
                     boiler_recipe.sprite = "fluid/" .. output.filter.name
                 end
                 local products = {{type="fluid", name=product_name, amount=product_amount,
