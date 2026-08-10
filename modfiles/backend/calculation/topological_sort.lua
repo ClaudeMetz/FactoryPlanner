3b7d283eeba66a6c5238cfbc0707784290144f8d
Vlad-Andrei Popescu <vladandreipopescu97@gmail.com>
2026-08-10 19:17:14 +0300

    Fix issue where order is not preserved

--- modfiles/backend/calculation/topological_sort.lua
+++ modfiles/backend/calculation/topological_sort.lua
@@ -1,6 +1,7 @@
 local _topological_sort = {}
 
 ---@class SortNode
+---@field id ObjectID
 ---@field products table<SolverItemKey, integer>
 ---@field ingredients table<SolverItemKey, integer>
 ---@field visited integer?
@@ -8,50 +9,53 @@ local _topological_sort = {}
 ---@class SortEdge
 ---@field item_key SolverItemKey
 ---@field cost number
+---@field source_node_id integer
 
 
 ---@param floor Floor
 ---@return ObjectID[]
 function _topological_sort.sort_floor(floor)
     -- Collect the products/ingredients on each line
-    local line_node = {}  ---@type table<ObjectID, SortNode>
+    local line_nodes = {}  ---@type table<integer, SortNode>
     for line_object in floor:iterator() do
-        line_node[line_object.id] = { products = {}, ingredients = {} }  ---@type SortNode
+        local line_node = { id = line_object.id, products = {}, ingredients = {} }  ---@type SortNode
         if line_object.class == "Floor" then
             local product_index = 1
             for _, item in pairs(line_object.products) do
                 local item_key = solver.util.pack_item(item.proto.name, item.proto.type)
-                line_node[line_object.id].products[item_key] = product_index
+                line_node.products[item_key] = product_index
                 product_index = product_index + 1
             end
             for _, item in pairs(line_object.byproducts) do
                 local item_key = solver.util.pack_item(item.proto.name, item.proto.type)
-                line_node[line_object.id].products[item_key] = product_index
+                line_node.products[item_key] = product_index
                 product_index = product_index + 1
             end
             local ingredient_index = 1
             for _, item in pairs(line_object.ingredients) do
                 local item_key = solver.util.pack_item(item.proto.name, item.proto.type)
-                line_node[line_object.id].ingredients[item_key] = ingredient_index
+                line_node.ingredients[item_key] = ingredient_index
                 ingredient_index = ingredient_index + 1
             end
         elseif line_object.class == "Line" then
             local product_index = 1
             for _, item in pairs(line_object.recipe.products) do
                 local item_key = solver.util.pack_item(item.name, item.type)
-                line_node[line_object.id].products[item_key] = product_index
+                line_node.products[item_key] = product_index
                 product_index = product_index + 1
             end
             local ingredient_index = 1
             for _, item in pairs(line_object.recipe.ingredients) do
                 local item_name = line_object.recipe:get_name_with_temperature(item)
                 local item_key = solver.util.pack_item(item_name, item.type)
-                line_node[line_object.id].ingredients[item_key] = ingredient_index
+                line_node.ingredients[item_key] = ingredient_index
                 ingredient_index = ingredient_index + 1
             end
 
             -- TODO: add fuel, power, heat, pollution
         end
+
+        table.insert(line_nodes, line_node)
     end
 
     local order = {}   ---@type ObjectID[]
@@ -59,34 +63,36 @@ function _topological_sort.sort_floor(floor)
     local iteration = 1
 
     --- Treat lines as nodes and items as `product -> ingredient` edges
-    ---@param line_id ObjectID
+    ---@param index integer
     ---@param path SortEdge[]
     ---@return SortEdge? cycling_edge
-    local function depth_first_search(line_id, path)
-        local node = line_node[line_id]
+    local function depth_first_search(index, path)
+        local node = line_nodes[index]
 
         if not node.visited then
             node.visited = iteration  -- mark as visited
         elseif node.visited < iteration then
             return  -- already sorted
         else -- loop detected
-            local weakest_edge = {item_key = "", cost = 0}  ---@type SortEdge
+            local weakest_edge = {item_key = "", cost = 0, source_node_id = 0}  ---@type SortEdge
             for i = #path, 1, -1 do
                 local edge = path[i]
                 if edge.cost > weakest_edge.cost then weakest_edge = edge end
+                if edge.source_node_id == node.id then break end
             end
 
+            cycled_items[weakest_edge.item_key] = true
             return weakest_edge
         end
 
 
         -- Visit all the neighbours (depth first)
         for item_key, product_index in pairs(node.products) do
-            for neighbor_id, neighbor_node in pairs(line_node) do
-                if neighbor_id ~= line_id then
+            for neighbor_index, neighbor_node in pairs(line_nodes) do
+                if neighbor_index ~= index then
                     local ingredient_index = neighbor_node.ingredients[item_key]
                     if ingredient_index then
-                        local edge = { item_key = item_key, cost = 0.0 }  ---@as SortEdge
+                        local edge = { item_key = item_key, cost = 0.0, source_node_id = node.id }  ---@as SortEdge
 
                         -- Calculate the cost heuristic for this edge
                         -- Assume no more than 1000 products/ingredients per floor
@@ -97,10 +103,11 @@ function _topological_sort.sort_floor(floor)
                         end
 
                         table.insert(path, edge)
-                        local cycling_edge = depth_first_search(neighbor_id, path)
+                        local cycling_edge = depth_first_search(neighbor_index, path)
+                        table.remove(path)
 
                         -- In the case of cycling, break the cycle on the selected edge
-                        if cycling_edge and cycling_edge ~= table.remove(path) then
+                        if cycling_edge and cycling_edge ~= edge then
                             node.visited = nil
                             return cycling_edge
                         end
@@ -111,12 +118,12 @@ function _topological_sort.sort_floor(floor)
 
         -- All the neighbors (if any, excuding cycles) have been added to the sorted list
         -- Add this node as well
-        table.insert(order, line_id)
+        table.insert(order, node.id)
     end
 
-    for line_id, node in pairs(line_node) do
+    for index, node in pairs(line_nodes) do
         if not node.visited then
-            depth_first_search(line_id, {})
+            depth_first_search(index, {})
             iteration = iteration + 1
         end
     end
