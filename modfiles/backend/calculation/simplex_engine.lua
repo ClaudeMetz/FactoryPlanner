996df2482659f6e650b4e156dc2092a215af53c2
Vlad-Andrei Popescu <vladandreipopescu97@gmail.com>
2026-09-11 11:46:16 +0300

    Fix merge error

--- modfiles/backend/calculation/simplex_engine.lua
+++ modfiles/backend/calculation/simplex_engine.lua
@@ -324,20 +324,20 @@ function simplex_engine.get_line_metadata(line_data, floor_id)
 
     -- Add other special categories
     if power_amount > 0 then
-        local item = { name = "custom-electric-power", type = "entity", amount = 0 }  ---@as SolverItem
+        local item = {name="custom-electric-power", type="entity", amount=0}  ---@as SolverItem
         structures.map.add(ingredients, item, power_amount)
     end
     if heat_amount > 0 then
-        local item = { name = "custom-heat-power", type = "entity", amount = 0 }  ---@as SolverItem
+        local item = {name="custom-heat-power", type="entity", amount=0}  ---@as SolverItem
         structures.map.add(ingredients, item, heat_amount)
     end
     if heating_amount > 0 then
-        local item = { name = "custom-heating-power", type = "entity", amount = 0 }  ---@as SolverItem
+        local item = {name="custom-heating-power", type="entity", amount=0}  ---@as SolverItem
         local item_key = structures.pack_item(item)
-        solver.util.table.add(ingredients, item_key, heating_amount)
+        structures.map.add(ingredients, item, heating_amount)
     end
     if line_data.pollutant_type and emissions ~= 0 then
-        local item = { name = "custom-" .. line_data.pollutant_type, type = "entity", amount = 0 }  ---@as SolverItem
+        local item = {name="custom-"..line_data.pollutant_type, type = "entity", amount = 0 }  ---@as SolverItem
         if emissions > 0 then
             structures.map.add(products, item, emissions)
         else
