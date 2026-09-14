6144d422d64625950eb7484459ae65a1cbd9c519
Vlad-Andrei Popescu <vladandreipopescu97@gmail.com>
2026-09-14 22:34:43 +0300

    Remove old line data from simplex

--- modfiles/backend/calculation/simplex_engine.lua
+++ modfiles/backend/calculation/simplex_engine.lua
@@ -218,7 +218,7 @@ function simplex_engine.update_factory(factory_data, result)
         end
     end
 
-    simplex_engine.update_floor(factory_data.top_floor, 1, top_byproducts, factory_data.line_data_map, result)
+    simplex_engine.update_floor(factory_data, factory_data.top_floor_id, 1, top_byproducts, result)
 
     solver.set_factory_result{
         player_index = factory_data.player_index,
@@ -230,37 +230,38 @@ function simplex_engine.update_factory(factory_data, result)
     }
 end
 
----@param floor_data OldFloorData
+---@param factory_data FactoryData
+---@param floor_id ObjectID
 ---@param scale_factor number
 ---@param byproducts SolverMap
----@param line_data_map LineDataMap
 ---@param result SimplexResult?
 ---@return integer machine_amount
-function simplex_engine.update_floor(floor_data, scale_factor, byproducts, line_data_map, result)
+function simplex_engine.update_floor(factory_data, floor_id, scale_factor, byproducts, result)
+    local floor_data = factory_data.floor_data_map[floor_id]
     local machine_amount = 0
 
-    for _, line_object_data in pairs(floor_data.lines) do
-        if not line_object_data.subfloor then
-            local line_result = result and result.line_results[line_object_data.id]
-            local line_machines = simplex_engine.update_line(floor_data.id,
-                    line_object_data, scale_factor, byproducts, line_data_map, line_result)
+    for _, line_object_id in pairs(floor_data.line_ids) do
+        if not factory_data.floor_data_map[line_object_id] then  -- Line
+            local line_result = result and result.line_results[line_object_id]
+            local line_data = factory_data.line_data_map[line_object_id]
+            local line_machines = simplex_engine.update_line(floor_id, line_data, scale_factor, byproducts, line_result)
             machine_amount = machine_amount + math.ceil(line_machines - MAGIC_NUMBERS.margin_of_error)
-        else
-            local subfloor_result = result and result.floor_results[line_object_data.id] or {
-                floor_id = line_object_data.id,
+        else  -- Floor
+            local subfloor_result = result and result.floor_results[line_object_id] or {
+                floor_id = line_object_id,
                 products = {},
                 ingredients = {},
             }
-            local line_result = result and result.line_results[line_object_data.id]
+            local line_result = result and result.line_results[line_object_id]
             local subfloor_scale_factor = (line_result and line_result.machine_amount or 0) * scale_factor
 
             local product_result, byproduct_result, ingredient_result, floor_byproducts =
                     simplex_engine.update_line_object_common(subfloor_scale_factor, subfloor_result.products, byproducts, subfloor_result.ingredients)
-            local floor_machines = simplex_engine.update_floor(line_object_data.subfloor, subfloor_scale_factor, floor_byproducts, line_data_map, result)
+            local floor_machines = simplex_engine.update_floor(factory_data, line_object_id, subfloor_scale_factor, floor_byproducts, result)
 
             solver.set_line_result{
-                floor_id = floor_data.id,
-                line_id = line_object_data.id,
+                floor_id = floor_id,
+                line_id = line_object_id,
                 machine_amount = floor_machines,
                 products = product_result,
                 byproducts = byproduct_result,
@@ -275,39 +276,35 @@ function simplex_engine.update_floor(floor_data, scale_factor, byproducts, line_
 end
 
 ---@param floor_id ObjectID
----@param line_data OldLineData
+---@param line_data LineData
 ---@param scale_factor number
 ---@param byproducts SolverMap
----@param line_data_map LineDataMap
 ---@param result SimplexLineResult?
 ---@return number machine_amount
-function simplex_engine.update_line(floor_id, line_data, scale_factor, byproducts, line_data_map, result)
-    local data = line_data_map[line_data.id]
-    if not data then return 0 end
-
+function simplex_engine.update_line(floor_id, line_data, scale_factor, byproducts, result)
     -- Update the machine
     local machine_amount = result and scale_factor * result.machine_amount or 0
-    local production_ratio = machine_amount * data.crafts_per_second
+    local production_ratio = machine_amount * line_data.crafts_per_second
 
     local product_result, byproduct_result, ingredient_result =
-            simplex_engine.update_line_object_common(machine_amount, data.products, byproducts, data.ingredients)
+            simplex_engine.update_line_object_common(machine_amount, line_data.products, byproducts, line_data.ingredients)
 
     -- Update the fuel
     local fuel_amount
-    if data.fuel_item then
-        local fuel_key = structures.pack_item(data.fuel_item)
-        fuel_amount = data.fuel_item.amount * machine_amount
+    if line_data.fuel_item then
+        local fuel_key = structures.pack_item(line_data.fuel_item)
+        fuel_amount = line_data.fuel_item.amount * machine_amount
         local ingredient_amount = ingredient_result[fuel_key] or 0
         if fuel_amount <= ingredient_amount then
-            structures.map.subtract(ingredient_result, data.fuel_item, fuel_amount)
+            structures.map.subtract(ingredient_result, line_data.fuel_item, fuel_amount)
         else
-            structures.map.add(product_result, data.fuel_item, fuel_amount - ingredient_amount)
+            structures.map.add(product_result, line_data.fuel_item, fuel_amount - ingredient_amount)
             ingredient_result[fuel_key] = nil
         end
     end
 
     solver.set_line_result{
-        line_id = line_data.id,
+        line_id = line_data.line_id,
         floor_id = floor_id,
         machine_amount = machine_amount,
         crafts_per_second = production_ratio,
