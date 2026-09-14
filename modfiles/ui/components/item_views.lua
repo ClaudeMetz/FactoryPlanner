06c4bf0f2f6a01a513dc0e34b78913b9553c9558
Therenas <claudearmetz@gmail.com>
2026-09-14 10:24:27 +0200

    Make each view prepare its own data

--- modfiles/ui/components/item_views.lua
+++ modfiles/ui/components/item_views.lua
@@ -25,14 +25,15 @@ end
 ---@return number? button_number
 ---@return LocalisedString tooltip
 function processors.throughput(metadata, raw_amount, item_proto, _)
+    local view = metadata.views.throughput  ---@as ThroughputViewData
     local raw_number, unit_name = nil, nil
 
     if item_proto.type == "fluid" then
-        raw_number = raw_amount / metadata.pumping_speed--[[@as number]]
+        raw_number = raw_amount / view.pumping_speed--[[@as number]]
         unit_name = "pump"
     else
-        raw_number = raw_amount * metadata.throughput_multiplier
-        unit_name = metadata.belts_or_lanes:sub(1, -2)
+        raw_number = raw_amount * view.multiplier
+        unit_name = view.belts_or_lanes:sub(1, -2)
     end
 
     local button_number = lib.format.button_number(raw_number)
@@ -91,8 +92,9 @@ end
 ---@return number? button_number
 ---@return LocalisedString tooltip
 function processors.wagons_per_timescale(metadata, raw_amount, item_proto, _)
-    local wagon_capacity = (item_proto.type == "fluid") and metadata.fluid_wagon_capacity
-        or metadata.cargo_wagon_capacity--[[@as number]] * item_proto.stack_size--[[@as uint]]
+    local view = metadata.views.wagons_per_timescale  ---@as WagonViewData
+    local wagon_capacity = (item_proto.type == "fluid") and view.fluid_capacity
+        or view.cargo_capacity--[[@as number]] * item_proto.stack_size--[[@as uint]]
     local raw_number = (raw_amount * metadata.timescale) / wagon_capacity
     local button_number = lib.format.button_number(raw_number)
 
@@ -110,7 +112,8 @@ end
 ---@return LocalisedString tooltip
 function processors.rockets_per_timescale(metadata, raw_amount, item_proto, _)
     if item_proto.type == "fluid" then return nil, {"fp.fluid_item"} end
-    local lift_capacity = metadata.lift_capacity--[[@as number]]
+    local view = metadata.views.rockets_per_timescale  ---@as RocketViewData
+    local lift_capacity = view.lift_capacity--[[@as number]]
     if item_proto.weight > lift_capacity then return nil, {"fp.item_too_heavy"} end
 
     local total_weight = raw_amount * metadata.timescale * item_proto.weight--[[@as Weight]]
@@ -161,13 +164,7 @@ end
 ---@field timescale Timescale
 ---@field timescale_string LocalisedString
 ---@field adjusted_margin_of_error number
----@field belts_or_lanes BeltsOrLanes
----@field throughput_multiplier number
 ---@field formatting_precision integer
----@field pumping_speed number?
----@field lift_capacity number?
----@field cargo_wagon_capacity number?
----@field fluid_wagon_capacity number?
 
 ---@class ItemViewData
 ---@field index integer
@@ -175,6 +172,18 @@ end
 ---@field tooltip LocalisedString?
 ---@field unavailable boolean?
 ---@field unavailable_for table<string, boolean>?
+
+---@class ThroughputViewData: ItemViewData
+---@field belts_or_lanes BeltsOrLanes
+---@field multiplier number
+---@field pumping_speed number?
+
+---@class WagonViewData: ItemViewData
+---@field cargo_capacity number?
+---@field fluid_capacity number?
+
+---@class RocketViewData: ItemViewData
+---@field lift_capacity number?
 
 ---@param default DefaultPrototype
 ---@return LuaEntityPrototype prototype
@@ -211,23 +220,21 @@ local function disable_unavailable_views(player)
 end
 
 ---@param player LuaPlayer
----@return ItemViewData view
----@return number throughput_multiplier
----@return number? pumping_speed
+---@return ThroughputViewData view
 local function prepare_throughput_view(player)
     local preferences = lib.globals.preferences(player)
     local belt = defaults.get(player, "belts").proto  ---@as FPBeltPrototype
     local belts_or_lanes, belt_stack = preferences.belts_or_lanes, preferences.belt_stack
     local divisor = (belts_or_lanes == "belts") and belt.throughput or (belt.throughput / 2)
-    local multiplier = (1 / divisor) / belt_stack
     local unit = {"fp.pl_" .. belts_or_lanes:sub(1, -2), 2}
     local stack_insert = (belt_stack > 1) and {"", {"fp.throughput_insert", belt_stack}, " "} or ""
 
     local pump = defaults.get_optional(player, "pumps")
-    local view = {index=2, caption=belt.rich_text, unavailable_for={fluid=not pump}}  ---@type ItemViewData
+    local view = {index=2, caption=belt.rich_text, unavailable_for={fluid=not pump},
+        belts_or_lanes=belts_or_lanes, multiplier=(1 / divisor) / belt_stack}  ---@type ThroughputViewData
     if not pump then
         view.tooltip = {"fp.view_tt", {"fp.throughput_belts_only", unit, stack_insert, belt.rich_text, belt.localised_name}}
-        return view, multiplier
+        return view
     end
 
     ---@cast pump.proto FPPumpPrototype
@@ -235,37 +242,35 @@ local function prepare_throughput_view(player)
     view.caption = {"", belt.rich_text, " ", pump.proto.rich_text}
     view.tooltip = {"fp.view_tt", {"fp.throughput", unit, stack_insert, belt.rich_text, belt.localised_name,
         pump.proto.rich_text, pump.proto.localised_name, quality}}
-    return view, multiplier, proto.get_pumping_speed(pump.quality--[[@cast -nil]].name) * 60
+    view.pumping_speed = proto.get_pumping_speed(pump.quality--[[@cast -nil]].name) * 60
+    return view
 end
 
 ---@param player LuaPlayer
 ---@param timescale_string string
----@return ItemViewData view
----@return number? cargo_capacity
----@return number? fluid_capacity
+---@return WagonViewData view
 local function prepare_wagon_view(player, timescale_string)
     local cargo = defaults.get_optional(player, "wagons", "cargo-wagon")
     local fluid = defaults.get_optional(player, "wagons", "fluid-wagon")
     local view = {index=5, caption={"", {"fp.pu_wagon", 2}, "/", {"fp.unit_" .. timescale_string}},
-        unavailable=not cargo and not fluid, unavailable_for={item=not cargo, fluid=not fluid}}  ---@type ItemViewData
+        unavailable=not cargo and not fluid, unavailable_for={item=not cargo, fluid=not fluid}}  ---@type WagonViewData
     if view.unavailable then
         view.tooltip = {"fp.preference_no_default_prototype", {"fp.pl_wagon", 2}}
         return view
     end
 
-    local cargo_capacity, fluid_capacity  ---@type number?, number?
     local cargo_quality, fluid_quality = "", ""  ---@type LocalisedString, LocalisedString
     if cargo then
         ---@cast cargo.proto FPWagonPrototype
         local proto
         proto, cargo_quality = proto_and_quality_string(cargo)
-        cargo_capacity = proto.get_inventory_size(defines.inventory.cargo_wagon, cargo.quality--[[@cast -nil]].name)
+        view.cargo_capacity = proto.get_inventory_size(defines.inventory.cargo_wagon, cargo.quality--[[@cast -nil]].name)
     end
     if fluid then
         ---@cast fluid.proto FPWagonPrototype
         local proto
         proto, fluid_quality = proto_and_quality_string(fluid)
-        fluid_capacity = proto.get_fluid_capacity(fluid.quality--[[@cast -nil]].name)
+        view.fluid_capacity = proto.get_fluid_capacity(fluid.quality--[[@cast -nil]].name)
     end
 
     view.caption = {"", cargo and cargo.proto.rich_text or "", fluid and fluid.proto.rich_text or "",
@@ -280,17 +285,16 @@ local function prepare_wagon_view(player, timescale_string)
         view.tooltip = {"fp.view_tt", {"fp.wagons_per_timescale_single", {"fp." .. timescale_string},
             wagon.proto.rich_text, wagon.proto.localised_name, cargo and cargo_quality or fluid_quality}}
     end
-    return view, cargo_capacity, fluid_capacity
+    return view
 end
 
 ---@param player LuaPlayer
 ---@param timescale_string string
----@return ItemViewData view
----@return number? lift_capacity
+---@return RocketViewData view
 local function prepare_rocket_view(player, timescale_string)
     local silo = defaults.get_optional(player, "silos")
     local view = {index=6, caption={"", "[img=fp_silo_rocket]", "/", {"fp.unit_" .. timescale_string}},
-        unavailable=not silo}  ---@type ItemViewData
+        unavailable=not silo}  ---@type RocketViewData
     if not silo then
         view.tooltip = {"fp.preference_no_default_prototype", {"fp.pl_silo", 2}}
         return view
@@ -300,17 +304,14 @@ local function prepare_rocket_view(player, timescale_string)
     local _, quality = proto_and_quality_string(silo)
     view.tooltip = {"fp.view_tt", {"fp.rockets_per_timescale", {"fp." .. timescale_string},
         silo.proto.rich_text, silo.proto.localised_name, quality}}
-    return view, silo.proto.rocket_lift_weight
+    view.lift_capacity = silo.proto.rocket_lift_weight
+    return view
 end
 
 ---@param player LuaPlayer
 function item_views.rebuild_data(player)
     local preferences = lib.globals.preferences(player)
     local timescale_string = lib.gui.timescale_as_string(preferences.timescale)
-
-    local throughput_view, throughput_multiplier, pumping_speed = prepare_throughput_view(player)
-    local wagon_view, cargo_capacity, fluid_capacity = prepare_wagon_view(player, timescale_string)
-    local rocket_view, lift_capacity = prepare_rocket_view(player, timescale_string)
 
     lib.globals.ui_state(player).views_data = {
         views = {
@@ -319,7 +320,7 @@ function item_views.rebuild_data(player)
                 caption = {"", {"fp.pu_item", 2}, "/", {"fp.unit_" .. timescale_string}},
                 tooltip = {"fp.view_tt", {"fp.items_per_timescale", {"fp." .. timescale_string}}}
             },
-            throughput = throughput_view,
+            throughput = prepare_throughput_view(player),
             items_per_second_per_machine = {
                 index = 3,
                 caption = {"", {"fp.pu_item", 2}, "/", {"fp.unit_second"}, "/[img=fp_generic_assembler]"},
@@ -330,19 +331,13 @@ function item_views.rebuild_data(player)
                 caption = {"", "[img=fp_stack]", "/", {"fp.unit_" .. timescale_string}},
                 tooltip = {"fp.view_tt", {"fp.stacks_per_timescale", {"fp." .. timescale_string}}}
             },
-            wagons_per_timescale = wagon_view,
-            rockets_per_timescale = rocket_view
+            wagons_per_timescale = prepare_wagon_view(player, timescale_string),
+            rockets_per_timescale = prepare_rocket_view(player, timescale_string)
         },
         timescale = preferences.timescale,
         timescale_string = {"fp.unit_" .. timescale_string}--[[@as LocalisedString]],
         adjusted_margin_of_error = MAGIC_NUMBERS.margin_of_error / preferences.timescale,
-        belts_or_lanes = preferences.belts_or_lanes,
-        throughput_multiplier = throughput_multiplier,
-        formatting_precision = MAGIC_NUMBERS.formatting_precision,
-        pumping_speed = pumping_speed,
-        lift_capacity = lift_capacity,
-        cargo_wagon_capacity = cargo_capacity,
-        fluid_wagon_capacity = fluid_capacity
+        formatting_precision = MAGIC_NUMBERS.formatting_precision
     }  ---@as ItemViewsData
     disable_unavailable_views(player)
 end
